28103c45177071daf30dde8cc99a227737820a30
ArmandoGraterol <armandogrtrl@gmail.com>
2019-05-15 14:26:15 -0400

    Added components and styles for introduction

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <Header />
+    <Introduction />
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Introduction from './components/Introduction.vue'
 
 export default {
   name: 'app',
   components: {
-    Header
+    Header,
+    Introduction
   }
 }
 </script>
