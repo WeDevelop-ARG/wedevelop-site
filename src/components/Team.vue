b598f2b82a093809e45d1ad5bfe675b423e553b6
ArmandoGraterol <armandogrtrl@gmail.com>
2019-05-16 10:51:38 -0400

    Added join us subsection

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -4,6 +4,9 @@
     <ul class="member-list">
       <Member v-for="member in members" v-bind:key="member.id" v-bind:member="member" />
     </ul>
+    <h3>Want to join us?</h3>
+    <p>We love to meet new people and have their ideas come alive in the projects we work.<br>If you share our <span class="highlight-paragraph">passion of working creatively and professionally</span>, please email us your resume or LinkedIn profile to</p>
+    <h4>jobs@wedevelop.me</h4>
   </div>
 </template>
 
@@ -76,6 +79,31 @@ export default {
     line-height: 60px;
   }
 
+  h3 {
+    color: #272A40;
+    font-size: 32px;
+    font-weight: 900;
+    letter-spacing: 0.6px;
+    line-height: 40px;
+  }
+
+  h4 {
+    color: #45DA60;
+    font-size: 24px;
+    font-weight: 700;
+    letter-spacing: 0.8px;
+    line-height: 35px;
+  }
+
+  p {
+    margin: 0;
+    color: #A7AFC3;
+    font-size: 18px;
+    letter-spacing: 0.6px;
+    line-height: 35px;
+    text-align: center;
+  }
+
   .team {
     margin-top: 16.2em;
   }
@@ -86,5 +114,9 @@ export default {
     padding: 0;
     margin: 0;
     list-style-type: none;
+  }
+
+  .highlight-paragraph {
+    color: #272A40;
   }
 </style>
