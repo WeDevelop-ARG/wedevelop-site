1ea949da61e84e5b706a220b904b7cf507109298
ArmandoGraterol <armandogrtrl@gmail.com>
2019-12-26 14:04:41 -0300

    Added webpack configuration to create sitemap.xml

---
+++ src/paths.js
@@ -0,0 +1,8 @@
+const paths = [
+  {
+    path: '/',
+    name: 'Home'
+  }
+]
+
+module.exports = { default: paths }
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,15 @@
+const SitemapPlugin = require('sitemap-webpack-plugin').default
+const paths = require('./src/paths').default.map(r => r.path).filter(p => !p.match(/\*/))
+
 module.exports = {
-  publicPath: ''
+  publicPath: '',
+  configureWebpack: {
+    plugins: [
+      new SitemapPlugin('https://wedevelop.me/', paths, {
+        fileName: 'sitemap.xml',
+        lastMod: true,
+        changeFreq: 'monthly'
+      })
+    ]
+  }
 }
