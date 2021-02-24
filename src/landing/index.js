12b460e847b5ca85df1b085f12c56e92b6898979
Nahuel Dallacamina <nahudalla@gmail.com>
2021-02-23 21:12:42 -0300

    Add logo component

--- src/landing/index.js
+++ src/landing/index.js
@@ -1,5 +1,7 @@
+import Logo from '../main_app/components/Logo'
+
 function Landing () {
-  return <h1>Landing</h1>
+  return <Logo primaryColor='#000000' secondaryColor='#FFFFFF' />
 }
 
 export default Landing
