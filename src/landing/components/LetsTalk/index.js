cdf10d93396ccb1fb58207d95a21dda7e1163631
Nahuel Dallacamina <nahudalla@gmail.com>
2021-03-05 13:44:33 -0300

    Position footer elements

--- src/landing/components/LetsTalk/index.js
+++ src/landing/components/LetsTalk/index.js
@@ -13,17 +13,23 @@ function LetsTalk () {
           I am
           <select name='i_am' className={classes.selectStyles}>
             <option>a Startup</option>
+            <option>a Company</option>
+            <option>an Individual</option>
           </select>
         </label>
         <label>
           looking to
           <select name='looking_to' className={classes.selectStyles}>
             <option>validate an idea</option>
+            <option>design / validate an idea</option>
+            <option>partner up with a dev team</option>
+            <option>scale / change a product</option>
+            <option>talk to a representative</option>
           </select>
         </label>
-        <input type='text' name='name' placeholder='Name' className={classes.formStyles}/>
-        <input type='email' name='email' placeholder='Email' className={classes.formStyles}/>
-        <textarea name='message' placeholder='Your message' maxLength='120' className={classes.formStyles}/>
+        <input type='text' name='name' placeholder='Name' className={classes.formStyles} />
+        <input type='email' name='email' placeholder='Email' className={classes.formStyles} />
+        <textarea name='message' placeholder='Your message' maxLength='120' className={classes.formStyles} />
         <div aria-label='0 characters out of 120'>0 / 120</div>
         <button type='submit' className={classes.buttonStyles}>Send Message</button>
       </form>
