f78f99df3447774aacaa07c339f3af46ad1e8283
Nahuel Dallacamina <nahudalla@gmail.com>
2021-03-05 12:02:45 -0300

    Add padding to sections

--- src/landing/index.js
+++ src/landing/index.js
@@ -1,6 +1,8 @@
 import NavBar from 'main_app/components/NavBar'
 import Footer from 'main_app/components/Footer'
 
+import classes from './styles.module.scss'
+
 import Header from './components/Header'
 import WhatAreYouLookingFor from './components/WhatAreYouLookingFor'
 import OurTeam from './components/OurTeam'
@@ -10,7 +12,7 @@ function Landing () {
   return (
     <>
       <NavBar />
-      <article>
+      <article className={classes.article}>
         <Header />
         <WhatAreYouLookingFor />
         <OurTeam />
