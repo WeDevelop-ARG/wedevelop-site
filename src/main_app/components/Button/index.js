913cb684b00ef9744f50c8e9855637bf54aefae8
Nahuel Dallacamina <nahudalla@gmail.com>
2021-03-10 11:41:55 -0300

    Add button component

---
+++ src/main_app/components/Button/index.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import classnames from 'classnames'
+
+import useVariants from 'utils/use_variants'
+
+import classes from './styles.module.scss'
+
+function Button ({
+  as: ButtonComponent = 'button',
+  variant = 'primary',
+  type,
+  className,
+  children,
+  iconLeft,
+  iconRight,
+  ...props
+}) {
+  if (ButtonComponent === 'button') type = type ?? 'button'
+
+  className = classnames(
+    classes.button,
+    useVariants(classes, variant, { prefix: 'variant_' }),
+    useVariants(classes, ButtonComponent.toString(), { prefix: 'as_' }),
+    className
+  )
+
+  return (
+    <ButtonComponent
+      type={type}
+      className={className}
+      {...props}
+    >
+      {iconLeft && <div className={classes.iconLeft}>{iconLeft}</div>}
+      <span>{children}</span>
+      {iconRight && <div className={classes.iconRight}>{iconRight}</div>}
+    </ButtonComponent>
+  )
+}
+
+const variants = PropTypes.oneOf(['primary', 'disabled', 'link'])
+
+Button.propTypes = {
+  as: PropTypes.string,
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+  type: PropTypes.string,
+  iconLeft: PropTypes.node,
+  iconRight: PropTypes.node,
+  variant: PropTypes.oneOfType([
+    variants,
+    PropTypes.arrayOf(variants)
+  ])
+}
+
+export default Button
