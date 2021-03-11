e62caf0083edf27711de8f98c3b41e1ef6b40222
dvnahuel <delvallenahuel@gmail.com>
2021-03-11 16:04:41 -0300

    Added queries style for header section

--- src/landing/components/Header/index.js
+++ src/landing/components/Header/index.js
@@ -5,7 +5,7 @@ import classes from './styles.module.scss'
 function Header (props, ref) {
   return (
     <section ref={ref} className={classes.headerContainer}>
-      <h1>A human-centered company that gets things done <u>efficiently</u>.</h1>
+      <h1 className={classes.title}>A human-centered company that gets things done <u>efficiently</u>.</h1>
       <blockquote className={classes.clutchReviews}>
         WeDevelop was resourceful, unafraid of problems and blockers, creative, professional, and fast.
       </blockquote>
