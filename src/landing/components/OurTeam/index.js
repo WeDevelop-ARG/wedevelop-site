d2c52430fc47633ada0a54ff8ae8a00a83742a02
dvnahuel <delvallenahuel@gmail.com>
2021-03-12 11:56:42 -0300

    Fixed queries styles for Our team section

--- src/landing/components/OurTeam/index.js
+++ src/landing/components/OurTeam/index.js
@@ -5,9 +5,9 @@ import classes from './styles.module.scss'
 function OurTeam () {
   return (
     <section id='our_team' className={classes.teamContainer}>
-      <div className={classes.teamFlexItem}>
+      <div className={classes.flexItems}>
         <SVGIcon name='logo' variant='rocket' className={classes.rocket} />
-        <h2>Our Team</h2>
+        <h2 className={classes.mainTitle}>Our Team</h2>
         <p>
           We are friendly group of people with professionalism and good vibes.
         </p>
@@ -15,7 +15,7 @@ function OurTeam () {
           We want everyone to feel that <b>working with us is like working with friends.</b>
         </p>
       </div>
-      <div className={classes.teamFlexItem}>
+      <div className={classes.rigthContainer}>
         <h3 className={classes.titleStyles}>#PeopleFirst</h3>
         <p>
           We’re a people-first company.
