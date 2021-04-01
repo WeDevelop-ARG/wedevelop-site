142f4422911a3b6a17ea3329dfb6efecba1ea73e
Vanina Winnik <73620426+vwinnik@users.noreply.github.com>
2021-04-01 15:29:12 -0300

    Contact page (#79)
    * Replace contact-us with contact.
    
    * Reimplement contact page.
    
    * Add package and package-lock files.
    
    * Modify contact page.
    
    * Fix invalid anchor
    
    Co-authored-by: Nahuel Dallacamina Ortea <nahuel@wedevelop.me>

--- src/contact_us/index.js
+++ src/contact_us/index.js
@@ -1,7 +1,7 @@
 import NavBar from 'main_app/components/NavBar'
 import Footer from 'main_app/components/Footer'
+import { CalendlyEventListener, InlineWidget } from 'react-calendly'
 
-import ContactForm from './components/ContactForm'
 import Article from 'main_app/components/Article'
 
 import classes from './styles.module.scss'
@@ -12,7 +12,14 @@ function ContactUs () {
       <NavBar />
       <div className={classes.wrapper}>
         <Article className={classes.fillHeight}>
-          <ContactForm />
+          <section>
+            <p>GET IN TOUCH</p>
+            <h1>Schedule a call with us</h1>
+            <CalendlyEventListener onEventScheduled={console.log}>
+              <InlineWidget url='https://calendly.com/abel-osorio/30min?back=0' />
+            </CalendlyEventListener>
+            <p>or email us at <a href='mailto:info@wedevelop.me'>info@wedevelop.me</a></p>
+          </section>
         </Article>
         <Footer />
       </div>
