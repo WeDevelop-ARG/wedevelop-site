9c95cf9c855c159250a8e67e016787d05668b307
vwinnik <winnikvanina@gmail.com>
2021-04-05 21:31:37 -0300

    Remove Scrollball from calendlyWidget.

--- src/contact_us/index.js
+++ src/contact_us/index.js
@@ -18,11 +18,7 @@ function ContactUs () {
             <CalendlyEventListener onEventScheduled={console.log}>
               <InlineWidget url='https://calendly.com/abel-osorio/30min?back=0' />
             </CalendlyEventListener>
-<<<<<<< HEAD
-            <span>or email us at</span><a href=''><span>info@wedevelop.me</span></a>
-=======
             <p>or email us at <a href='mailto:info@wedevelop.me'>info@wedevelop.me</a></p>
->>>>>>> 142f4422911a3b6a17ea3329dfb6efecba1ea73e
           </section>
         </Article>
         <Footer />
