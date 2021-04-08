5ce34331df67a5ce20ed5569f5a158fa1defa1d3
Hiram J. González <hiram.gonzalez@wedevelop.me>
2021-04-08 11:01:01 -0400

    Add visuals to Staff Augmentation page

--- src/staff_augmentation/components/Header/index.js
+++ src/staff_augmentation/components/Header/index.js
@@ -1,16 +1,19 @@
 import ClutchWidget from 'main_app/components/ClutchWidget'
+import SVGIcon from 'main_app/components/SVGIcon'
 
 function Header () {
   return (
     <section>
       <div>
-        <img src='https://placehold.co/188x149' alt='' />
         <p>IT staff augmentation</p>
         <h1>
           <div>The extra talent needed</div>
           <div>to Boost <span>Your Projects</span></div>
         </h1>
         <ClutchWidget variant='dark' />
+      </div>
+      <div>
+        <SVGIcon name='staff_augmentation/flying_rocket' />
       </div>
     </section>
   )
