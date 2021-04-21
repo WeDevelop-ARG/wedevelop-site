e86dfc970c97929cf644cd1623457f48a1e47b23
Nahuel Dallacamina <nahudalla@gmail.com>
2021-04-21 20:39:05 -0300

    Setup GCF environment variables & add missing dependency

--- functions/constants.js
+++ functions/constants.js
@@ -1,9 +1,9 @@
 const functions = require('firebase-functions')
 const config = functions.config()
 
-exports.CALENDLY_ACCESS_TOKEN = config.calendly.accessToken
-exports.CALENDLY_API_BASE_URL = config.calendly.baseURL || 'https://api.calendly.com/'
+exports.CALENDLY_ACCESS_TOKEN = config.calendly.access_token
+exports.CALENDLY_API_BASE_URL = config.calendly.base_url || 'https://api.calendly.com/'
 
-exports.MAILCHIMP_API_KEY = config.mailchimp.apiKey
+exports.MAILCHIMP_API_KEY = config.mailchimp.api_key
 exports.MAILCHIMP_SERVER = config.mailchimp.server
-exports.MAILCHIMP_DEFAULT_LIST_ID = config.mailchimp.defaultListId
+exports.MAILCHIMP_DEFAULT_LIST_ID = config.mailchimp.default_list_id
