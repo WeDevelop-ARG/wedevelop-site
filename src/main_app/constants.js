1d284e2fc81c0dfe0f311d8592a4bc2029c25ef0
vwinnik <winnikvanina@gmail.com>
2021-04-22 14:56:09 -0300

    Modify constants

--- src/main_app/constants.js
+++ src/main_app/constants.js
@@ -1,10 +1,11 @@
-export const MAILER_URL = process.env.REACT_APP_MAILER_URL || 'http://localhost:5001/wedevelop-site/us-central1/processStaffAugmentationFormSubmit'
+export const MAILER_URL = process.env.REACT_APP_MAILER_URL || 'http://localhost:3000/send'
 export const ENVIRONMENT = process.env.REACT_APP_ENVIRONMENT || 'development'
 export const BASE_URL = process.env.REACT_APP_BASE_URL
 export const PROCESS_CALENDLY_EVENT_INVITEE_ENDPOINT_URL = (
   process.env.REACT_APP_CALENDLY_EVENT_PROCESSOR_URL ||
   'http://localhost:5001/wedevelop-site/us-central1/processCalendlyEventInvitee'
 )
+export const PROCESS_STAFF_AUGMENTATION_FORM_SUBMIT = 'http://localhost:5001/wedevelop-site/us-central1/processStaffAugmentationFormSubmit'
 export const IS_DEVELOPMENT = ENVIRONMENT === 'development'
 export const IS_TESTING = ENVIRONMENT === 'testing'
 export const IS_PRODUCTION = ENVIRONMENT === 'production'
