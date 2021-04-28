bb5f0add796cbce39076f3cd310469bd1a9f60e0
vwinnik <winnikvanina@gmail.com>
2021-04-28 09:15:36 -0300

    Modify ReCAPTCHAField component.

--- src/main_app/components/ReCAPTCHAField/index.js
+++ src/main_app/components/ReCAPTCHAField/index.js
@@ -5,15 +5,14 @@ import { RECAPTCHA_SITE_KEY } from 'main_app/constants'
 function ReCAPTCHAField ({ name, className }) {
   const containerRef = useRef()
   const widgetIdRef = useRef()
-  const grecaptchaObject = window.grecaptcha
   const { setFieldValue, setTouched, values } = useFormikContext()
   useEffect(() => {
     if (!values[name] && widgetIdRef.current) {
-      grecaptchaObject?.reset(widgetIdRef.current)
+      window.grecaptcha?.reset(widgetIdRef.current)
     }
   }, [name, values[name]])
   useEffect(() => {
-    widgetIdRef.current = grecaptchaObject?.render(containerRef.current, {
+    widgetIdRef.current = window.grecaptcha?.render(containerRef.current, {
       sitekey: RECAPTCHA_SITE_KEY,
       callback: token => {
         if (!token) return undefined
@@ -23,7 +22,7 @@ function ReCAPTCHAField ({ name, className }) {
     }
     )
     return () => {
-      grecaptchaObject?.reset(widgetIdRef.current)
+      window.grecaptcha?.reset(widgetIdRef.current)
       widgetIdRef.current = null
       while (containerRef.current.firstChild) {
         containerRef.current.removeChild(
