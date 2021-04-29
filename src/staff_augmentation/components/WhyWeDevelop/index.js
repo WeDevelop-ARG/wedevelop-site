b40f0556eb765460461f211744922767d0803fb6
Hiram J. González <hiram.gonzalez@wedevelop.me>
2021-04-29 16:31:30 -0400

    Apply styles to Why WeDevelop section

--- src/staff_augmentation/components/WhyWeDevelop/index.js
+++ src/staff_augmentation/components/WhyWeDevelop/index.js
@@ -1,28 +1,30 @@
 import SVGIcon from 'main_app/components/SVGIcon'
 
+import classes from './styles.module.scss'
+
 function WhyWeDevelop () {
   return (
-    <section>
-      <div>
-        <p>Why WeDevelop</p>
-        <h2>You are in <span>the right place if you</span></h2>
+    <section className={classes.whyWeDevelopSection}>
+      <div className={classes.sectionHeader}>
+        <p className={classes.subheadingText}>Why WeDevelop</p>
+        <h2 className={classes.titleText}>You are in <span>the right place if you</span></h2>
       </div>
-      <div>
-        <ul>
-          <li>
+      <div className={classes.sectionContent}>
+        <ul className={classes.list}>
+          <li className={classes.listItem}>
             <SVGIcon name='staff_augmentation/rocket_icon' />
-            <p>Want to <span>grow your company’s IT Staff quickly.</span></p>
+            <p>Want to <span className={classes.highlightedText}>grow your company’s IT Staff quickly.</span></p>
           </li>
-          <li>
+          <li className={classes.listItem}>
             <SVGIcon name='staff_augmentation/tie_icon' />
-            <p>Are looking for <span>Web Developers, Web Designers.</span></p>
+            <p>Are looking for <span className={classes.highlightedText}>Web Developers, Web Designers.</span></p>
           </li>
-          <li>
+          <li className={classes.listItem}>
             <SVGIcon name='staff_augmentation/target_icon' />
-            <p>Are tired of <span>Spending too much time on interviews.</span></p>
+            <p>Are tired of <span className={classes.highlightedText}>Spending too much time on interviews.</span></p>
           </li>
         </ul>
-        <SVGIcon name='staff_augmentation/insert_idea' />
+        <SVGIcon name='staff_augmentation/insert_idea' className={classes.insertIdea} />
       </div>
     </section>
   )
