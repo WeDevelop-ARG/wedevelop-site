9c6ffa4f67da723a7234aed23b7b65761ef01740
Hiram J. González <hiram.gonzalez@wedevelop.me>
2021-04-30 19:27:02 -0400

    Apply styles and media queries to Our Talents section

--- src/staff_augmentation/components/OurTalents/index.js
+++ src/staff_augmentation/components/OurTalents/index.js
@@ -1,37 +1,39 @@
 import SVGIcon from 'main_app/components/SVGIcon'
 
+import classes from './styles.module.scss'
+
 function OurTalents () {
   return (
-    <section>
-      <div>
-        <p>Our talents</p>
+    <section className={classes.ourTalentsSection}>
+      <div className={classes.sectionHeader}>
+        <p className={classes.subheadingText}>Our talents</p>
         <h2>Expertly Matched Talent</h2>
         <p>We’ll present you candidates in no time. You’ll see.</p>
       </div>
-      <div>
-        <div>
-          <SVGIcon name="staff_augmentation/developers_icon" />
-          <p>Developers</p>
-          <p>
-            Deliver high quality engineering solutions: 
-            <span>engineers, programmers, coders, architects, and more.</span>
+      <div className={classes.sectionContent}>
+        <div className={classes.cards}>
+          <SVGIcon name="staff_augmentation/developers_icon" className={classes.cardsIcon} />
+          <p className={classes.cardsTitle}>Developers</p>
+          <p className={classes.cardsText}>
+            Deliver high quality engineering solutions: <span className={classes.highlightedText}>engineers, 
+            programmers, coders, architects, and more.</span>
           </p>
         </div>
-        <div>
-          <SVGIcon name="staff_augmentation/designers_icon" />
-          <p>Designers</p>
-          <p>
+        <div className={classes.cards}>
+          <SVGIcon name="staff_augmentation/designers_icon" className={classes.cardsIcon} />
+          <p className={classes.cardsTitle}>Designers</p>
+          <p className={classes.cardsText}>
             Curate an seamless end-to-end design experience 
-            for your audience with our <span>UI, UX, Visual, and 
+            for your audience with our <span className={classes.highlightedText}>UI, UX, Visual, and 
             Interaction design experts.</span>
           </p>
         </div>
-        <div>
-          <SVGIcon name="staff_augmentation/leaders_icon" />
-          <p>Team Leaders</p>
-          <p>
-            Execute critical projects with experienced 
-            <span>team leaders and  lead your way to success.</span>
+        <div className={classes.cards}>
+          <SVGIcon name="staff_augmentation/leaders_icon" className={classes.cardsIcon} />
+          <p className={classes.cardsTitle}>Team Leaders</p>
+          <p className={classes.cardsText}>
+            Execute critical projects with experienced <span className={classes.highlightedText}>team 
+            leaders and  lead your way to success.</span>
           </p>
         </div>
       </div>
