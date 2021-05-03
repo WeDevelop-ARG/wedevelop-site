17c7788b2ff4d000a507a7a5373aa729d5e7509c
vwinnik <winnikvanina@gmail.com>
2021-05-03 11:16:11 -0300

    Move isReCAPTCHATokenValid

--- functions/controllers/processStaffAugmentationFormSubmit.js
+++ functions/controllers/processStaffAugmentationFormSubmit.js
@@ -26,6 +26,10 @@ function handleOptionsRequest (req, res) {
 }
 
 async function handlePostRequest (req, res) {
+  if (!await isReCAPTCHATokenValid(req.body.recaptchaToken)) {
+    return res.status(403).end()
+  }
+
   const subscriber = await addSubscriberToMailchimp({
     listId: MAILCHIMP_DEFAULT_LIST_ID,
     subscriber: { email: req.body.email }
@@ -37,9 +41,5 @@ async function handlePostRequest (req, res) {
     tags: getMailchimpTags('staff-augmentation')
   })
 
-  if (!await isReCAPTCHATokenValid(req.body.recaptchaToken)) {
-    return res.status(403).end()
-  } else {
-    return res.status(200).end()
-  }
+  return res.status(200).end()
 }
