ab025f0f297cdd7d2eb5270264fa2124dff89f7b
Hiram J. González <hiram.gonzalez@wedevelop.me>
2021-05-17 19:31:55 -0400

    Improve Modal generic styles

--- src/main_app/components/Modal/index.js
+++ src/main_app/components/Modal/index.js
@@ -1,16 +1,17 @@
 import ReactModal from 'react-modal'
+import classnames from 'classnames'
 
 import SVGIcon from 'main_app/components/SVGIcon'
 
 import classes from './styles.module.scss'
 
-function Modal ({ children, isOpen, onRequestClose }) {
+function Modal ({ children, className, isOpen, onRequestClose }) {
   return (
     <ReactModal 
       isOpen={isOpen}
       onRequestClose={onRequestClose}
       appElement={document.getElementById('root')}
-      className={classes.modal}
+      className={classnames(classes.modal, className)}
       overlayClassName={classes.overlay}
       portalClassName={classes.portal}
       contentLabel={'Confirmation Message'}
