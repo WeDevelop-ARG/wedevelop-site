0bbc04ea87a5f2c16814fbf02e5eb399ecdcb23d
vwinnik <winnikvanina@gmail.com>
2021-05-18 15:01:17 -0300

    Modify Tech Solutions section

--- src/staff_augmentation/components/DeliverTechSolutions/index.js
+++ src/staff_augmentation/components/DeliverTechSolutions/index.js
@@ -1,58 +1,95 @@
-import { HashLink } from 'react-router-hash-link'
 import classnames from 'classnames'
 
-import Button from 'main_app/components/Button'
 import MaleInVideocall from 'assets/staff_augmentation/male_videocall.png'
 import MaleUsingComputer from 'assets/staff_augmentation/male_computer.png'
+import MaleSalesSolutions from 'assets/staff_augmentation/male_sales_solutions.png'
 
 import classes from './styles.module.scss'
 
 function DeliverTechSolutions () {
   return (
     <section className={classes.techSolutionsSection}>
-      <div className={classes.bigCircle} role='presentation' />
-      <div className={classes.smallCircle} role='presentation' />
       <div className={classnames(classes.contentWrapper, classes.firstContentWrapper)}>
         <div className={classes.maleVideocallFrame}>
           <img src={MaleInVideocall} alt='' role='presentation' className={classes.maleVideocallImage} />
         </div>
         <div className={classnames(classes.textContainer, classes.firstTextContainer)}>
-          <h2>We Deliver scalable <span>Tech Solutions</span></h2>
+          <h2>Meet your next
+            <span className={classes.gradientText}> Web Developer today</span>
+          </h2>
           <p>
-            We are the expert partner you need to deliver scalable, 
-            innovative, and competitive results.We specialize in solid 
-            end-to-end <span>delivery of tailor-made technology solutions.</span>
+            We send you a list of candidates today along
+            with their rates and easy-to-read resumes.
+            <span>
+              Your time is too precious to be dealing with
+              Recruiting, Screening, Selection,
+              Interviewing, Hiring, and more.
+            </span>
           </p>
-          <Button
-            as={HashLink}
-            to='#header-section'
-            smooth
-            variant={'secondary'}
-            className={classes.upgradeButton}
-          >
-            Upgrade your team
-          </Button>
+          <p>
+            Let us alleviate that burden. Save time.
+            Grow Efficiently.
+          </p>
         </div>
       </div>
       <div className={classes.contentWrapper}>
+        <div className={classes.textContainer}>
+        <div className={classnames(classes.textContainer, classes.firstTextContainer)}>
+          <h2 className={classes.titleText}>Hire Top Web Developers, <span className={classes.gradientText}>Without Hassle</span></h2>
+          <p>
+            Hiring the right developer is hard. Especially
+            if you're looking for someone with specific
+            skills and experience in a particular
+            language or technology.
+          </p>
+          <p>
+            At, WeDevelop, we match developers to
+            clients based on skill sets, interests, and
+            location & provide several options that meet
+            your requirements.
+            Hire top talent from around In Latin America
+            without having to go through the hassle of
+            interviewing them yourself.
+          </p>
+          <p>
+            Focus on other important
+            things such as growing
+            your business and meeting deadlines.
+          </p>
+        </div>
+        </div>
         <div className={classes.maleComputerFrame}>
           <img src={MaleUsingComputer} alt='' role='presentation' className={classes.maleComputerImage} />
         </div>
+      </div>
+      <div className={classes.contentWrapper}>
+        <div className={classes.maleVideocallFrame}>
+          <img src={MaleSalesSolutions} alt='' role='presentation' className={classes.maleVideocallImage} />
+        </div>
         <div className={classes.textContainer}>
-          <h2 className={classes.titleText}>We Deliver scalable Tech Solutions</h2>
+        <div className={classnames(classes.textContainer, classes.firstTextContainer)}>
+          <h2 className={classes.titleText}>Save time, <span className={classes.gradientText}>grow faster</span></h2>
           <p>
-            Our experienced engineering teams deliver dream-team 
-            outcomes, <span>without the hassles of hiring.</span>
+            Hiring the wrong developers can cost
+            thousands of dollars and hours of
+            wasted time.
           </p>
-          <Button
-            as={HashLink}
-            to='#header-section'
-            smooth
-            variant={'secondary'}
-            className={classes.upgradeButton}
-          >
-            Upgrade your team
-          </Button>
+          <p>
+            At WeDevelop, we've already put
+            in the groundwork.
+          </p>
+          <p>
+            We have access to the best developers for
+            your company’s needs, allowing you to focus
+            on growing your business instead of trying
+            to find new staff members.
+          </p>
+          <p>
+            Stop spending time and resources on
+            Interviews. If you want to grow your
+            company’s IT Staff quickly
+          </p>
+        </div>
         </div>
       </div>
     </section>
