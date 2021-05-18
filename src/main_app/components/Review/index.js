035824c2a0e1e6252c984daf86895fcd8eda41f8
Hiram J. González <hiram.gonzalez@wedevelop.me>
2021-05-18 00:51:17 -0400

    Add Review generic component

---
+++ src/main_app/components/Review/index.js
@@ -0,0 +1,19 @@
+import Stars from './Stars'
+
+import classes from './styles.module.scss'
+
+function Review ({ quote, stars, project }) {
+  return (
+    <div className={classes.review}>
+      <p className={classes.quote}>
+        {quote}
+      </p>
+      <Stars starsNumber={stars} />
+      <p className={classes.project}>
+        {project}
+      </p>
+    </div>
+  )
+}
+
+export default Review
