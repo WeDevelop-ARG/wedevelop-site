f6850c2f55a58d974db5701840a3c3082f4901c9
vwinnik <winnikvanina@gmail.com>
2021-05-18 18:44:00 -0300

    Add responsive Tech Solutions section

--- src/staff_augmentation/components/DeliverTechSolutions/index.js
+++ src/staff_augmentation/components/DeliverTechSolutions/index.js
@@ -14,8 +14,8 @@ function DeliverTechSolutions () {
           <img src={MaleInVideocall} alt='' role='presentation' className={classes.maleVideocallImage} />
         </div>
         <div className={classnames(classes.textContainer, classes.firstTextContainer)}>
-          <h2>Meet your next
-            <span className={classes.gradientText}> Web Developer today</span>
+          <h2>Meet your next <br />
+            <span className={classes.gradientText}>Web Developer today</span>
           </h2>
           <p>
             We send you a list of candidates today along
@@ -34,62 +34,62 @@ function DeliverTechSolutions () {
       </div>
       <div className={classes.contentWrapper}>
         <div className={classes.textContainer}>
-        <div className={classnames(classes.textContainer, classes.firstTextContainer)}>
-          <h2 className={classes.titleText}>Hire Top Web Developers, <span className={classes.gradientText}>Without Hassle</span></h2>
-          <p>
-            Hiring the right developer is hard. Especially
-            if you're looking for someone with specific
-            skills and experience in a particular
-            language or technology.
-          </p>
-          <p>
-            At, WeDevelop, we match developers to
-            clients based on skill sets, interests, and
-            location & provide several options that meet
-            your requirements.
-            Hire top talent from around In Latin America
-            without having to go through the hassle of
-            interviewing them yourself.
-          </p>
-          <p>
-            Focus on other important
-            things such as growing
-            your business and meeting deadlines.
-          </p>
-        </div>
+          <div className={classnames(classes.textContainer, classes.firstTextContainer)}>
+            <h2 className={classes.titleText}>Hire Top Web Developers, <br /><span className={classes.gradientText}>Without Hassle</span></h2>
+            <p>
+              Hiring the right developer is hard. Especially
+              if you're looking for someone with specific
+              skills and experience in a particular
+              language or technology.
+            </p>
+            <p>
+              At, WeDevelop, we match developers to
+              clients based on skill sets, interests, and
+              location & provide several options that meet
+              your requirements.
+              Hire top talent from around In Latin America
+              without having to go through the hassle of
+              interviewing them yourself.
+            </p>
+            <p>
+              Focus on other important
+              things such as growing
+              your business and meeting deadlines.
+            </p>
+          </div>
         </div>
         <div className={classes.maleComputerFrame}>
           <img src={MaleUsingComputer} alt='' role='presentation' className={classes.maleComputerImage} />
         </div>
       </div>
-      <div className={classes.contentWrapper}>
+      <div className={classnames(classes.contentWrapper, classes.hiddenTextContainer)}>
         <div className={classes.maleVideocallFrame}>
           <img src={MaleSalesSolutions} alt='' role='presentation' className={classes.maleVideocallImage} />
         </div>
         <div className={classes.textContainer}>
-        <div className={classnames(classes.textContainer, classes.firstTextContainer)}>
-          <h2 className={classes.titleText}>Save time, <span className={classes.gradientText}>grow faster</span></h2>
-          <p>
-            Hiring the wrong developers can cost
-            thousands of dollars and hours of
-            wasted time.
-          </p>
-          <p>
-            At WeDevelop, we've already put
-            in the groundwork.
-          </p>
-          <p>
-            We have access to the best developers for
-            your company’s needs, allowing you to focus
-            on growing your business instead of trying
-            to find new staff members.
-          </p>
-          <p>
-            Stop spending time and resources on
-            Interviews. If you want to grow your
-            company’s IT Staff quickly
-          </p>
-        </div>
+          <div className={classnames(classes.textContainer, classes.firstTextContainer)}>
+            <h2 className={classes.titleText}>Save time, <span className={classes.gradientText}>grow faster</span></h2>
+            <p>
+              Hiring the wrong developers can cost
+              thousands of dollars and hours of
+              wasted time.
+            </p>
+            <p>
+              At WeDevelop, we've already put
+              in the groundwork.
+            </p>
+            <p>
+              We have access to the best developers for
+              your company’s needs, allowing you to focus
+              on growing your business instead of trying
+              to find new staff members.
+            </p>
+            <p>
+              Stop spending time and resources on
+              Interviews. If you want to grow your
+              company’s IT Staff quickly
+            </p>
+          </div>
         </div>
       </div>
     </section>
