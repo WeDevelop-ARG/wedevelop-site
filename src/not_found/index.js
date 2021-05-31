645a8742137d0a873092dd599da1c72830e173d9
Hiram J. González <hiram.gonzalez@wedevelop.me>
2021-05-31 02:09:28 -0400

    Apply styles and add some visuals

--- src/not_found/index.js
+++ src/not_found/index.js
@@ -4,9 +4,12 @@ import { HashLink } from 'react-router-hash-link'
 import NavBar from 'main_app/components/NavBar'
 import Article from 'main_app/components/Article'
 import Button from 'main_app/components/Button'
+import SVGIcon from 'main_app/components/SVGIcon'
 
 import usePageMedatada from 'utils/marketing/use_page_metadata'
 import useOverlappingObserver from 'utils/use_overlapping_observer'
+
+import classes from './styles.module.scss'
 
 function NotFound () {
   usePageMedatada({
@@ -28,19 +31,22 @@ function NotFound () {
     <>
       <NavBar ref={navBarRef} variant={navBarVariant} hideMenu />
       <Article>
-        <section>
-          <h1>404</h1>
-          <h2>Houston, We have a problem!</h2>
-          <p>The page you're looking for is lost in cyberspace...</p>
-          <Button
-            as={HashLink}
-            isAnchor
-            variant='secondary'
-            to='/'
-            smooth
-          >
-            Let's head back to earth
-          </Button>
+        <section className={classes.notFoundSection}>
+          <div className={classes.textContainer}>
+            <h1>404</h1>
+            <h2>Houston, we have a problem!</h2>
+            <p>The page you're looking for is lost in cyberspace...</p>
+            <Button
+              as={HashLink}
+              isAnchor
+              variant='secondary'
+              to='/#top'
+              smooth
+            >
+              Let's head back to earth
+            </Button>
+          </div>
+          <SVGIcon name='not_found/astronaut' className={classes.astronaut} />
         </section>
       </Article>
     </>
