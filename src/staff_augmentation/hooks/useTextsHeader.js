3fee306585fb07102d753e70bce1951c3f1b093e
vwinnik <winnikvanina@gmail.com>
2021-06-01 14:43:41 -0300

    Add useTextsHeader hook

---
+++ src/staff_augmentation/hooks/useTextsHeader.js
@@ -0,0 +1,16 @@
+import EntrepreneurLaptop from 'assets/staff_augmentation/entrepreneur_laptop.png'
+
+const TEXTS_HEADER = [{
+  title: <>Hire top web developers <span>in 48h</span></>,
+  description: 'Get access to the top Software Engineers in Latin America.',
+  highlightedText: 'Fast, without the hiring hassles.',
+  availableDetails: <span>Sign Up to Get Rates &amp; Available Staff Details</span>,
+  subTextButton: 'We typically reply every email within an hour.',
+  presentationPhotoURL: EntrepreneurLaptop
+}]
+
+function useTextsHeader () {
+  return TEXTS_HEADER
+}
+
+export default useTextsHeader
