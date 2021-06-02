406560c9bcff663f455ff45433ff39f8230c781b
Nahuel Dallacamina <nahudalla@gmail.com>
2021-06-01 21:31:02 -0300

    Remove unnecessary array

--- src/hooks/useGetYourQuote.js
+++ src/hooks/useGetYourQuote.js
@@ -1,9 +1,9 @@
-const YOUR_QUOTE = [{
+const YOUR_QUOTE = {
   subHeading: 'Get your quote now',
   title: 'Ready to grow your IT Staff?',
   nameButton: 'Get your quote now',
   subTextButton: 'We typically reply every email within an hour.'
-}]
+}
 
 function useGetYourQuote () {
   return YOUR_QUOTE
--- src/staff_augmentation/components/GetYourQuote/index.js
+++ src/staff_augmentation/components/GetYourQuote/index.js
@@ -6,35 +6,32 @@ import Button from 'main_app/components/Button'
 import classes from './styles.module.scss'
 
 function GetYourQuote () {
-  const textsGetYourQuote = usegetYourQuote()
+  const {
+    subHeading,
+    title,
+    nameButton,
+    subTextButton
+  } = usegetYourQuote()
+
   return (
     <section className={classes.getYourQuoteSection}>
-      {textsGetYourQuote.map((
-        {
-          index,
-          subHeading,
-          title,
-          nameButton,
-          subTextButton
-        }) => (
-          <div className={classes.container} key={index}>
-            <div className={classes.leftColumn}>
-              <h3 className={classes.subheadingText}>{subHeading}</h3>
-              <h2>{title}</h2>
-            </div>
-            <div className={classes.rightColumn}>
-              <Button
-                as={HashLink}
-                to='#top'
-                smooth
-                className={classes.getYourQuoteButton}
-              >
-                {nameButton}
-              </Button>
-              <div className={classes.subText}>{subTextButton}</div>
-            </div>
-          </div>
-      ))}
+      <div className={classes.container}>
+        <div className={classes.leftColumn}>
+          <h3 className={classes.subheadingText}>{subHeading}</h3>
+          <h2>{title}</h2>
+        </div>
+        <div className={classes.rightColumn}>
+          <Button
+            as={HashLink}
+            to='#top'
+            smooth
+            className={classes.getYourQuoteButton}
+          >
+            {nameButton}
+          </Button>
+          <div className={classes.subText}>{subTextButton}</div>
+        </div>
+      </div>
     </section>
   )
 }
