41b860713f4fece8992a75fe201b34095cdeb8a3
Nahuel Dallacamina <nahudalla@gmail.com>
2021-06-02 12:18:54 -0300

    Add review quotes using CSS

--- src/staff_augmentation/hooks/useLandingVariants.js
+++ src/staff_augmentation/hooks/useLandingVariants.js
@@ -26,22 +26,22 @@ const variants = [{
     title: 'The top IT Staff Augmentation Agency so they say...',
     reviews: [
       {
-        quote: '“They’re a well-rounded team, which is conducive to troubleshooting [and] having difficult conversations.”',
+        quote: 'They’re a well-rounded team, which is conducive to troubleshooting [and] having difficult conversations.',
         stars: 5,
         project: 'App Development for Online Fitness Company'
       },
       {
-        quote: '“They’re willing to engage with a problem to figure it out rather than waiting to be told what to do.”',
+        quote: 'They’re willing to engage with a problem to figure it out rather than waiting to be told what to do.',
         stars: 5,
         project: 'Software Development for Sports Betting Startup'
       },
       {
-        quote: '"The project cost about half of what it would have had we hired local engineers."',
+        quote: 'The project cost about half of what it would have had we hired local engineers.',
         stars: 5,
         project: 'Web App Development for Loan Company'
       },
       {
-        quote: '"WeDevelop was resourceful, unafraid of problems and blockers, creative, professional, and fast."',
+        quote: 'WeDevelop was resourceful, unafraid of problems and blockers, creative, professional, and fast.',
         stars: 5,
         project: 'Product Development for Ferry Booking Platform'
       }
