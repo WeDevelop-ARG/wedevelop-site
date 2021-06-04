9529750fc807054d35bdc2d925c02ac7525447a5
vwinnik <winnikvanina@gmail.com>
2021-06-04 11:29:34 -0300

    Modify JoinUs styles

--- src/home/components/JoinUs/index.js
+++ src/home/components/JoinUs/index.js
@@ -6,9 +6,9 @@ function JoinUs () {
       <h3 className={classes.subTitle}>Want to join us?</h3>
       <p className={classes.description}>
         We love to meet new people and have their ideas come alive in the projects we work.<br />If you share our
-        <span>
+        <b>
           <> passion of working creatively and professionally</>
-        </span>
+        </b>
         , please email us your resume or LinkedIn profile to
       </p>
       <h4 className={classes.email}>jobs@wedevelop.me</h4>
