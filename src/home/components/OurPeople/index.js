a75ff5c7ef74ed536d7921fcb0790d58b62ab89c
Hiram J. González <hiram.gonzalez@wedevelop.me>
2021-06-07 16:15:21 -0400

    Use TextGradient component in section title

--- src/home/components/OurPeople/index.js
+++ src/home/components/OurPeople/index.js
@@ -1,3 +1,4 @@
+import BrandGradient from 'main_app/components/TextGradient'
 import IntroductionImg from 'assets/home/introduction-img.svg'
 
 import classes from './styles.module.scss'
@@ -6,7 +7,7 @@ function OurPeople () {
   return (
     <div id='our-people' className={classes.ourPeopleSection}>
       <div className={classes.textContainer}>
-        <h2>We are <span className={classes.gradientText}>Friendly and Professional</span> people</h2>
+        <h2>We are <BrandGradient>Friendly and Professional</BrandGradient> people</h2>
         <p>WeDevelop is a SDaaS company born in Argentina focused on <b>high-quality and long-term solutions.</b></p>
         <p>Clients’ satisfaction is our goal, so we strive to always provide excellent software following the <b>best practices</b> and pursuing <b>cutting-edge technologies.</b></p>
         <p>We are also friendly people with professionalism and good vibes and we want everyone to feel that <b>working with us is like working with friends.</b></p>
