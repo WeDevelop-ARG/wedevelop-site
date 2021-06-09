3e7b6997e1c09d57cbc4e275192e00ad4e14c9c1
Nahuel Dallacamina Ortea <nahuel@wedevelop.me>
2021-06-09 11:06:09 -0300

    Remove unnecessary fragment

--- src/home/components/JoinUs/index.js
+++ src/home/components/JoinUs/index.js
@@ -6,9 +6,7 @@ function JoinUs () {
       <h3 className={classes.subTitle}>Want to join us?</h3>
       <p className={classes.description}>
         We love to meet new people and have their ideas come alive in the projects we work.<br />If you share our
-        <b>
-          <> passion of working creatively and professionally</>
-        </b>
+        <b>{' '}passion of working creatively and professionally</b>
         , please email us your resume or LinkedIn profile to
       </p>
       <h4 className={classes.email}>jobs@wedevelop.me</h4>
