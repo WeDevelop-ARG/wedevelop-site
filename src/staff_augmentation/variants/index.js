8c16397774901f2e16d02e79855a7fc1c55455cc
vwinnik <winnikvanina@gmail.com>
2021-06-15 12:19:35 -0300

    Add web_designers to variants

--- src/staff_augmentation/variants/index.js
+++ src/staff_augmentation/variants/index.js
@@ -1,9 +1,11 @@
 import staffAugmentation from './staff_augmentation'
+import webDesigners from './web_designers'
 import webDevelopers from './web_developers'
 
 const variants = [
   staffAugmentation,
-  webDevelopers
+  webDevelopers,
+  webDesigners
 ]
 
 export default variants
