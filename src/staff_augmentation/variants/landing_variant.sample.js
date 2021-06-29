be39266a76231186b80b4de49bca3289468453b5
Hiram J. González <hiram.gonzalez@wedevelop.me>
2021-06-29 10:59:43 -0400

    Create landing variant sample file

---
+++ src/staff_augmentation/variants/landing_variant.sample.js
@@ -0,0 +1,62 @@
+const landing_variant = {
+  pathName: '',
+  metadata: {
+    title: '',
+    description: ''
+  },
+  header: {
+    title: <></>,
+    description: (
+      <></>
+    ),
+    formDescription: '',
+    formOrigin: '',
+    formCustomizations: {
+      fixedFieldsPlaceholders: {
+        name: '',
+        email: '',
+        message: ''
+      },
+      customFields: [
+        {
+          name: '',
+          placeholder: '',
+          initialValue: '',
+          required: false
+        }
+      ]
+    },
+    sideImageURL: '',
+    submitButtonText: ''
+  },
+  reviews: {
+    title: '',
+    reviews: [
+      {
+        id: '',
+        quote: '',
+        stars: 0,
+        project: ''
+      }
+    ]
+  },
+  sectionsWithSideImage: [
+    {
+      id: '',
+      imageURL: '',
+      title: <></>,
+      description: (
+        <></>
+      )
+    }
+  ],
+  fullSizeCTA: {
+    title: '',
+    subTitle: '',
+    ctaText: '',
+    ctaToPath: '',
+    ctaIncentive: ''
+  }
+}
+
+export default landing_variant
