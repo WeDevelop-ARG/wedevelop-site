0476ee263d3cc0a1c8c25c08910b6a9e71956391
Hiram J. González <hiram.gonzalez@wedevelop.me>
2021-07-15 16:34:26 -0400

    Change Contact Modal structure for styling purposes

--- src/main_app/components/ContactModal/index.js
+++ src/main_app/components/ContactModal/index.js
@@ -8,27 +8,29 @@ function ContactModal ({ isOpen, onRequestClose }) {
   return (
     <Modal
       className={classes.contactModal}
-      isOpen={isOpen}
+      isOpen
       onRequestClose={onRequestClose}
       contentLabel='Schedule a call'
     >
-      <section className={classes.modalContent}>
+      <div className={classes.modalHeader}>
         <p className={classes.subheadingText}>GET IN TOUCH</p>
         <h1 className={classes.titleText}>Schedule a call with us</h1>
         <p className={classes.descriptionText}>We look forward to learning how WeDevelop can help you and your business</p>
         <hr className={classes.horizontalBar} />
+      </div>
+      <section className={classes.calendlyWidget}>
         <CalendlyWidget sourcePage='contact' />
-        <div className={classes.btnContainer}>
-          <Button
-            isAnchor
-            variant='secondary'
-            className={classes.cancelButton}
-            onClick={onRequestClose}
-          >
-            Cancel
-          </Button>
-        </div>
       </section>
+      <div className={classes.btnContainer}>
+        <Button
+          isAnchor
+          variant='secondary'
+          className={classes.cancelButton}
+          onClick={onRequestClose}
+        >
+          Cancel
+        </Button>
+      </div>
     </Modal>
   )
 }
