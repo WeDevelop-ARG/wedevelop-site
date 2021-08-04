7ad5caecfaf7621ab2d0c4a4d6a4c4e216433336
vwinnik <winnikvanina@gmail.com>
2021-08-04 10:19:23 -0300

    Add margin to social media icons

--- src/main_app/components/Footer/index.js
+++ src/main_app/components/Footer/index.js
@@ -109,17 +109,17 @@ function Footer ({ variant = 'dark', className }) {
         <ul className={classes.socialNetworks}>
           <li>
             <a href='https://www.linkedin.com/company/wedevelop-arg' target='_blank' rel='noreferrer'>
-              <SVGIcon name='footer/linked_in' title={'WeDevelop\'s LinkedIn'} className={classes.iconStyles} />
+              <SVGIcon name='footer/linked_in' title={'WeDevelop\'s LinkedIn'} className={classes.iconSocialNetworks} />
             </a>
           </li>
           <li>
             <a href='https://facebook.com/WeDevelop-101618141565362' target='_blank' rel='noreferrer'>
-              <SVGIcon name='footer/facebook' title={'WeDevelop\'s Facebook'} className={classes.iconStyles} />
+              <SVGIcon name='footer/facebook' title={'WeDevelop\'s Facebook'} className={classes.iconSocialNetworks} />
             </a>
           </li>
           <li>
             <a href='https://www.instagram.com/wedevelop.me' target='_blank' rel='noreferrer'>
-              <SVGIcon name='footer/instagram' title={'WeDevelop\'s Instagram'} className={classes.iconStyles} />
+              <SVGIcon name='footer/instagram' title={'WeDevelop\'s Instagram'} className={classes.iconSocialNetworks} />
             </a>
           </li>
         </ul>
