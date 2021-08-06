158511a495088d4bd48ab8b41f84f0cdec8b2807
vwinnik <winnikvanina@gmail.com>
2021-08-06 14:06:21 -0300

    Change images of services section

--- src/home/components/Service/index.js
+++ src/home/components/Service/index.js
@@ -1,10 +1,9 @@
 import classes from './styles.module.scss'
-import SVGIcon from 'main_app/components/SVGIcon'
 
 function Service ({ title, description, photo, path }) {
   return (
     <div>
-      <SVGIcon className={classes.photo} name={photo} />
+      <img className={classes.photo} src={photo} alt='' />
       <h3 className={classes.nameService}>{title}</h3>
       <p className={classes.description}>{description}</p>
     </div>
