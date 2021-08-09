4898edbd2e0f087dd993093f49866cd8a680a896
Hiram J. González <hiram.gonzalez@wedevelop.me>
2021-08-09 13:30:27 -0400

    Extract generic components from Home page

--- src/home/index.js
+++ src/home/index.js
@@ -3,15 +3,17 @@ import { useRouteMatch, useHistory } from 'react-router-dom'
 
 import NavBar from 'main_app/components/NavBar'
 import Article from 'main_app/components/Article'
+import ContactModal from 'main_app/components/ContactModal'
 import Header from './components/Header'
 import AboutUs from './components/AboutUs'
 import Services from './components/Services'
-import Testimonials from './components/Testimonials'
-import GetInTouch from './components/GetInTouch'
+import Testimonials from 'main_app/components/Testimonials'
+import GetInTouch from 'main_app/components/GetInTouch'
+import PictureWall from 'main_app/components/PictureWall'
 import Footer from 'main_app/components/Footer'
 
 import usePageMetadata from 'utils/marketing/use_page_metadata'
-import ContactModal from 'main_app/components/ContactModal'
+import TestimonialsBg from 'assets/home/testimonials_background_image.png'
 
 function Landing () {
   usePageMetadata({
@@ -34,8 +36,9 @@ function Landing () {
         <Header />
         <Services />
         <AboutUs />
-        <Testimonials />
+        <Testimonials bottomImageURL={TestimonialsBg} />
         <GetInTouch />
+        <PictureWall />
       </Article>
       {match?.isExact && <ContactModal isOpen onRequestClose={handleClose} />}
       <Footer variant='light' />
