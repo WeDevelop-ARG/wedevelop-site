4ee9efe6876bc5bfc739137e401ff1cc37a06571
vwinnik <winnikvanina@gmail.com>
2021-08-12 16:20:23 -0300

    Add About Us link in Navbar

--- src/main_app/components/NavBar/MainMenu/index.js
+++ src/main_app/components/NavBar/MainMenu/index.js
@@ -19,6 +19,7 @@ function MainMenu ({
 
   return (
     <ul className={classnames(classes.menu, className, { [classes.hidden]: !isOpen })}>
+      <li className={classnames(classes.blogText)}><a href='/about-us' target='_blank' rel='noopener noreferrer'>About Us</a></li>
       <li className={classnames(classes.blogText)}><a href='https://blog.wedevelop.me' target='_blank' rel='noopener noreferrer'>Blog</a></li>
       <li className={classnames(classes.textList)}>Let's talk and work together</li>
       <li>
