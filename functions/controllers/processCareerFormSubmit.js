e2dd808d14afaccbd605b64fe67be1cd46f87e19
Hiram J. González <hiram.gonzalez@wedevelop.me>
2021-08-24 16:46:20 -0400

    Set up cloud function to send Career Form data to Mailchimp

---
+++ functions/controllers/processCareerFormSubmit.js
@@ -0,0 +1,96 @@
+const {
+  addApplicantToMailchimp,
+  addDetailsToMailchimpApplicant
+} = require('../services/mailchimp')
+const { isReCAPTCHATokenValid } = require('../services/recaptcha')
+const { sendEmail } = require('../services/sendEmail')
+
+const { MAILCHIMP_DEFAULT_LIST_ID, CONTACT_FORM_DESTINATION_EMAIL } = require('../constants')
+
+module.exports = exports = async function handleRequest (req, res) {
+  res.set('Access-Control-Allow-Origin', '*')
+  try {
+    switch (req.method) {
+      case 'OPTIONS': return await handleOptionsRequest(req, res)
+      case 'POST': return await handlePostRequest(req, res)
+      default: res.status(405).end()
+    }
+  } catch (err) {
+    console.error(err)
+    res.status(500).end()
+  }
+}
+
+function handleOptionsRequest (req, res) {
+  res.set('Access-Control-Allow-Methods', 'POST')
+  res.set('Access-Control-Allow-Headers', 'Content-Type')
+  res.set('Access-Control-Max-Age', '3600')
+  res.status(204).end()
+}
+
+async function handlePostRequest (req, res) {
+  const {
+    firstName,
+    lastName,
+    email,
+    skills,
+    remuneration,
+    message,
+    resume,
+    recaptchaToken
+  } = req.body
+
+  const emailMessage = `
+      New application received from Join Us form, Career page.
+
+      - First name: ${firstName}
+      - Last name: ${lastName}
+      - Email: ${email}
+      - Skills: ${skills}
+      - Intended remuneration (monthly): ${remuneration}
+      - About me: ${message}
+      - Resume: ${resume}
+  `
+
+  const applicantDetails = `
+    - Skills: ${skills}
+    - Intended remuneration (monthly): ${remuneration}
+    - About me: ${message}
+    - Resume: ${resume}
+  `
+
+  const data = {
+    personalizations: [{
+      to: [{ email: CONTACT_FORM_DESTINATION_EMAIL }],
+      subject: 'New application from WeDevelop site'
+    }],
+    from: { email: CONTACT_FORM_DESTINATION_EMAIL },
+    content: [{
+      type: 'text/plain',
+      value: emailMessage
+    }]
+  }
+
+  if (!await isReCAPTCHATokenValid(recaptchaToken)) {
+    return res.status(403).end()
+  }
+
+  await sendEmail(data)
+
+  const applicant = await addApplicantToMailchimp({
+    listId: MAILCHIMP_DEFAULT_LIST_ID,
+    applicant: {
+      email: email,
+      firstName: firstName,
+      lastName: lastName
+    }
+  })
+
+  await addDetailsToMailchimpApplicant({
+    listId: MAILCHIMP_DEFAULT_LIST_ID,
+    applicantId: applicant.id,
+    note: applicantDetails
+  })
+
+  return res.status(200).end()
+}
