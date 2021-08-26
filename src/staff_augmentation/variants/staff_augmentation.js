a602a5108355bbdc758124f35504649e2e009e73
Hiram J. González <hiram.gonzalez@wedevelop.me>
2021-08-26 16:26:16 -0400

    Adapt variants to new Customers Reviews structure

--- src/staff_augmentation/variants/staff_augmentation.js
+++ src/staff_augmentation/variants/staff_augmentation.js
@@ -31,27 +31,41 @@ const staffAugmentation = {
     submitButtonText: 'Get a free quote'
   },
   reviews: {
+    subtitle: 'Customers reviews',
     title: 'The top IT Staffing as our customers say',
+    description: 'Duis ornare nunc sed molestie consectetur nulla eget. Tincidunt curabitur.',
     reviews: [{
       id: '1',
       quote: 'They’re a well-rounded team, which is conducive to troubleshooting [and] having difficult conversations.',
       stars: 5,
-      project: 'App Development for Online Fitness Company'
+      project: {
+        type: 'App Development',
+        company: 'Online Fitness Company'
+      }
     }, {
       id: '2',
       quote: 'They’re willing to engage with a problem to figure it out rather than waiting to be told what to do.',
       stars: 5,
-      project: 'Software Development for Sports Betting Startup'
+      project: {
+        type: 'Software Development',
+        company: 'Sports Betting Startup'
+      }
     }, {
       id: '3',
       quote: 'The project cost about half of what it would have had we hired local engineers.',
       stars: 5,
-      project: 'Web App Development for Loan Company'
+      project: {
+        type: 'Web App Development',
+        company: 'Loan Company'
+      }
     }, {
       id: '4',
       quote: 'WeDevelop was resourceful, unafraid of problems and blockers, creative, professional, and fast.',
       stars: 5,
-      project: 'Product Development for Ferry Booking Platform'
+      project: {
+        type: 'Product Development',
+        company: 'Ferry Booking Platform'
+      }
     }]
   },
   sectionsWithSideImage: [{
