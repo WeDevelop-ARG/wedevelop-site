74063736512797e35120bd2bd592b0cf7488e9f1
Hiram J. González <hiram.gonzalez@wedevelop.me>
2021-08-26 16:23:15 -0400

    Refactor Customers Reviews section

--- src/staff_augmentation/components/ReviewCards/index.js
+++ src/staff_augmentation/components/ReviewCards/index.js
@@ -1,11 +1,21 @@
+import { HashLink } from 'react-router-hash-link'
+
 import Review from './Review'
+import Button from 'main_app/components/Button'
+
+import DotsPattern from 'assets/staff_augmentation/dots_pattern.svg'
 
 import classes from './styles.module.scss'
 
-function ReviewCards ({ title, reviews }) {
+function ReviewCards ({ subtitle, title, description, reviews }) {
   return (
     <section id='reviews-section' className={classes.reviewsSection}>
-      <h3 className={classes.titleText}>{title}</h3>
+      <div className={classes.sectionHeader}>
+        <p className={classes.subheadingText}>{subtitle}</p>
+        <h2 className={classes.titleText}>{title}</h2>
+        <p className={classes.descriptionText}>{description}</p>
+        <hr className={classes.horizontalBar} />
+      </div>
       <div className={classes.cardsContainer}>
         {
           reviews.map(({ id, quote, stars, project }) => (
@@ -18,6 +28,21 @@ function ReviewCards ({ title, reviews }) {
           ))
         }
       </div>
+      <Button
+        as={HashLink}
+        to='#'
+        smooth
+        isAnchor
+        variant='primary'
+        className={classes.freeQuoteButton}
+      >
+        Get a Free Quote
+      </Button>
+      <img src={DotsPattern} alt='' className={classes.topLeftDotsPattern} aria-hidden='true' />
+      <div className={classes.filledCircle} aria-hidden='true' />
+      <div className={classes.emptyCircle} aria-hidden='true' />
+      <div className={classes.smallBlurredCircle} aria-hidden='true' />
+      <div className={classes.smallCircle} aria-hidden='true' />
     </section>
   )
 }
