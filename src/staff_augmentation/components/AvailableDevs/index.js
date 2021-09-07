6a54b575c37e01aee71b2c5a592c64d70a4f1375
Hiram J. González <hiram.gonzalez@wedevelop.me>
2021-09-07 17:24:41 -0400

    Adjust Available Team section to new design

--- src/staff_augmentation/components/AvailableDevs/index.js
+++ src/staff_augmentation/components/AvailableDevs/index.js
@@ -4,36 +4,50 @@ import Button from 'main_app/components/Button'
 import ListRow from './ListRow'
 
 import useRandonNumber from './use_random_number'
+import DotsPattern from 'assets/hire_developers/dots_pattern.svg'
 
 import classes from './styles.module.scss'
 
-function AvailableDevs ({ title, devs }) {
+function AvailableDevs ({ subtitle, title, description, devs, buttonText, handleModalClose }) {
   return (
     <section className={classes.availableDevs}>
-      <h2 className={classes.title}>{title}</h2>
+      <div className={classes.sectionHeader}>
+        <p className={classes.subheadingText}>{subtitle}</p>
+        <h2 className={classes.titleText}>{title}</h2>
+        <p className={classes.descriptionText}>{description}</p>
+        <hr className={classes.horizontalBar} />
+      </div>
       <div className={classes.devsList}>
-        {devs.map(({ id, devImgURL, devRole, techLogoURL, devDescription, devRate }) => (
+        {devs.map(({ id, devImgURL, devName, devRole, devSkills, devExperience, devRate }) => (
           <ListRow
             key={id}
             devImgURL={devImgURL}
+            devName={devName}
             devRole={devRole}
-            techLogoURL={techLogoURL}
-            devDescription={devDescription}
+            devSkills={devSkills}
+            devExperience={devExperience}
             devRate={devRate}
           />
         ))}
         <div className={classes.moreOnBench}>
-          <p>{useRandonNumber(6, 15)} more on bench</p>
+          <p>and more than <b>{useRandonNumber(6, 20)} awesome professionals</b> waiting for you!</p>
         </div>
       </div>
       <Button
         as={HashLink}
-        to='#top'
+        to=''
         smooth
+        variant='primary'
         className={classes.getRatesButton}
+        onClick={handleModalClose}
       >
-        Get rates & profiles
+        {buttonText}
       </Button>
+      <div className={classes.filledCircle} aria-hidden='true' />
+      <div className={classes.emptySolidCircle} aria-hidden='true' />
+      <div className={classes.emptyLightCircle} aria-hidden='true' />
+      <div className={classes.smallBlurCircle} aria-hidden='true' />
+      <img src={DotsPattern} alt='' className={classes.dotsPattern} />
     </section>
   )
 }
