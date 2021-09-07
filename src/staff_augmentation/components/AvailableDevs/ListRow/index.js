3c7d798c46a8c22fecc5e3a0b187bb0db4f1d144
Hiram J. González <hiram.gonzalez@wedevelop.me>
2021-09-07 17:28:00 -0400

    Work on refactoring Dev List Row component

--- src/staff_augmentation/components/AvailableDevs/ListRow/index.js
+++ src/staff_augmentation/components/AvailableDevs/ListRow/index.js
@@ -1,32 +1,44 @@
+import { HashLink as Link } from 'react-router-hash-link'
+
 import Button from 'main_app/components/Button'
 import SVGIcon from 'main_app/components/SVGIcon'
-import { HashLink as Link } from 'react-router-hash-link'
 
 import classes from './styles.module.scss'
 
 function ListRow ({
   devImgURL,
+  devName,
   devRole,
-  techLogoURL,
-  devDescription,
+  devSkills,
+  devExperience,
   devRate
 }) {
   return (
     <ul className={classes.devRow}>
-      <li>
-        <img src={devImgURL} alt='' className={classes.profileImg} />
+      <li className={classes.devImgName}>
+        <div className={classes.profileImgContainer}>
+          <img src={devImgURL} alt='' className={classes.profileImg} />
+        </div>
+        <p>{devName}</p>
       </li>
       <li className={classes.roleTech}>
         <b>{devRole}</b>
-        <img src={techLogoURL} alt='' className={classes.techLogo} />
+      </li>
+      <li className={classes.skills}>
+        {devSkills.map(({ skill, color }) => (
+          <span key={skill}>{skill}</span>
+        ))}
       </li>
       <li className={classes.description}>
-        {devDescription}
+        {devExperience}
       </li>
-      <li className={classes.rateMagnifier}>
-        <p>$<span className={classes.profileRate}>{devRate}</span> / h</p>
-        <Button variant='icon' as={Link} isAnchor smooth to='#top' className={classes.searchButton}>
-          <SVGIcon name='hire_developers/search_solid' className={classes.magnifier} />
+      <li className={classes.rate}>
+        <p>{devRate}</p>
+      </li>
+      <li className={classes.viewProfile}>
+        <p className={classes.viewProfileText}>View Profile</p>
+        <Button variant='icon' as={Link} isAnchor smooth to='#top'>
+          <SVGIcon name='hire_developers/right_arrow' className={classes.rightArrow} />
         </Button>
       </li>
     </ul>
