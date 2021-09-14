522a2e732ad689b92d72055c0c72b01d250e761a
Hiram J. González <hiram.gonzalez@wedevelop.me>
2021-09-14 17:04:49 -0400

    Create Our Process section

--- src/services/components/ourProcess/index.js
+++ src/services/components/ourProcess/index.js
@@ -1,19 +1,94 @@
 import React from 'react'
-import classes from './ourProcess.module.scss'
+
+import Discovery from 'assets/services/web_dev_our_process/discovery.svg'
+import InitialBacklog from 'assets/services/web_dev_our_process/initial_backlog.svg'
+import Proposal from 'assets/services/web_dev_our_process/proposal.svg'
+import ProjectStarts from 'assets/services/web_dev_our_process/project_starts.svg'
+import ProcessDiagram from 'assets/services/web_dev_our_process/process_diagram.svg'
+import DotsPattern from 'assets/services/dots-pattern.svg'
+
+import classes from './styles.module.scss'
 
 function OurProcess () {
   return (
-    <>
-      <div className={classes.container}>
-        <div className={classes.content}>
-          <h2 className={classes.title}>Our Process</h2>
-          <p>TBD</p>
+    <section id='our-process' className={classes.ourProcess}>
+      <div className={classes.sectionHeader}>
+        <p className={classes.subheadingText}>Agile Methodologies</p>
+        <h2 className={classes.titleText}>Our Process</h2>
+        <p className={classes.descriptionText}>
+          At WeDevelop we use the Agile methodology to deliver working software every Sprint.
+        </p>
+        <hr className={classes.horizontalBar} />
+      </div>
+      <div className={classes.sectionContent}>
+        <div className={classes.bulletsList}>
+          <div className={classes.phaseContainer}>
+            <div className={classes.phaseIdentifier}>
+              <img src={Discovery} alt='' />
+              <p>Discovery</p>
+            </div>
+            <ul className={classes.phaseSteps}>
+              <li>
+                Get to know your needs
+              </li>
+              <li>
+                Evalute project’s viability and feasibility
+              </li>
+              <li>
+                Learn about our process in detail
+              </li>
+            </ul>
+          </div>
+          <div className={classes.phaseContainer}>
+            <div className={classes.phaseIdentifier}>
+              <img src={InitialBacklog} alt='' />
+              <p>Initial Backlog Definition &amp; Estimation</p>
+            </div>
+            <ul className={classes.phaseSteps}>
+              <li>
+                Breakdown the initial project into clear &amp; estimable work pieces
+              </li>
+              <li>
+                In joint workshops estimate the initial backlog using techniques
+                such us Poker Planning &amp; Magic Estimation
+              </li>
+            </ul>
+          </div>
+          <div className={classes.phaseContainer}>
+            <div className={classes.phaseIdentifier}>
+              <img src={Proposal} alt='' />
+              <p>Proposal</p>
+            </div>
+            <ul className={classes.phaseSteps}>
+              <li>
+                We’ll prepare and send an Agile fixed price proposal according to
+                the Backlog created in collaboration with you and your team
+              </li>
+            </ul>
+          </div>
+          <div className={classes.phaseContainer}>
+            <div className={classes.phaseIdentifier}>
+              <img src={ProjectStarts} alt='' />
+              <p>Project Starts!</p>
+            </div>
+            <ul className={classes.phaseSteps}>
+              <li>
+                We specialize in Agile Software Development, which is highly
+                collaborative and eficient
+              </li>
+              <li>
+                First functional delivery in the first 2 sprints. Guaranteeded
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={classes.processDiagramContainer}>
+          <img src={ProcessDiagram} alt='' className={classes.processDiagram} />
         </div>
       </div>
-      <div className={classes.shadowLine} />
       <div className={classes.background} />
-    </>
-
+      <img src={DotsPattern} className={classes.dotsPatternRight} alt='' />
+    </section>
   )
 }
 
