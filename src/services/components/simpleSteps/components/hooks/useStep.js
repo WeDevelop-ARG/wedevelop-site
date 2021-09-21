ef4cdc73139aa932b3a370dc1bddf73392128698
Abel M. Osorio <abel.m.osorio@gmail.com>
2021-09-21 12:17:57 -0300

    Rewording on the SimpleSteps component.

--- src/services/components/simpleSteps/components/hooks/useStep.js
+++ src/services/components/simpleSteps/components/hooks/useStep.js
@@ -1,18 +1,18 @@
 const steps = [
   {
     number: 1,
-    title: 'Requirements and needs discussion',
+    title: 'Requirements',
     description: 'Let’s figure out what you need. Share your ideas and expectations with us, and we’ll help you define your needs.'
   },
   {
     number: 2,
     title: 'Talent proposal',
-    description: 'We’ll create a talent proposal. I line with your budget and with candidates that have gone through a rigorous hiring process by our recruiters and technical experts.'
+    description: 'We’ll send you a list of candidates and their rates within the next 24 hours.'
   },
   {
     number: 3,
-    title: 'Meetings and Legal Aspects',
-    description: 'Last step. Time to negotiate our partnership and discuss legal aspects.'
+    title: 'Evaluate and Hire!',
+    description: 'Evaluate the candidates we send you and hire the one you like best! As simple as that!'
   }
 ]
 
