653b5eee702429e20ba5b755e7216c821597d560
Hiram J. González <hiram.gonzalez@wedevelop.me>
2021-09-22 15:31:46 -0400

    Adapt Web Dev Benefits section to responsive design

--- src/services/components/benefits/components/DecorationWebDev.js
+++ src/services/components/benefits/components/DecorationWebDev.js
@@ -1,14 +1,18 @@
-import patternVertical from 'assets/services/dots-pattern.svg'
-import patternHorizontal from 'assets/services/dots-pattern-horizontal.svg'
-import circles from 'assets/services/benefits-deco-circles.svg'
+import PatternVertical from 'assets/services/dots-pattern.svg'
+import PatternHorizontal from 'assets/services/dots-pattern-horizontal.svg'
+import Circles from 'assets/services/benefits-deco-circles.svg'
+import LeftSemicircleMobile from 'assets/services/benefits/left_semicircle_mobile.svg'
+import RightSemicircleMobile from 'assets/services/benefits/right_semicircle_mobile.svg'
 import classes from './decoration.module.scss'
 
 function DecorationWebDev() {
   return (
     <>
-      <img className={classes.webDevPatternVertical} src={patternVertical} alt="" />
-      <img className={classes.webDevPatternHorizontal} src={patternHorizontal} alt="" />
-      <img className={classes.webDevCircles} src={circles} alt="" />
+      <img className={classes.webDevPatternVertical} src={PatternVertical} alt='' aria-hidden />
+      <img className={classes.webDevPatternHorizontal} src={PatternHorizontal} alt='' aria-hidden />
+      <img className={classes.webDevCircles} src={Circles} alt='' aria-hidden />
+      <img className={classes.webDevLeftSemicircleMobile} src={LeftSemicircleMobile} alt='' aria-hidden />
+      <img className={classes.webDevRightSemicircleMobile} src={RightSemicircleMobile} alt='' aria-hidden />
     </>
 
   )
