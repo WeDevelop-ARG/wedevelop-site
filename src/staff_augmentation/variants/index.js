494043b79566e39bbec0b5a9ceb522164f6a4c21
Abel M. Osorio <abel.m.osorio@gmail.com>
2021-10-06 10:46:17 -0300

    New variant hire-javascript-developers.

--- src/staff_augmentation/variants/index.js
+++ src/staff_augmentation/variants/index.js
@@ -2,12 +2,14 @@ import staffAugmentation from './staff_augmentation'
 import webDevelopers from './web_developers'
 import webCoders from './web_coders'
 import hireDevelopers from './hire_developers'
+import hireJavascriptDevelopers from './hire_javascript_developers'
 
 const variants = [
   staffAugmentation,
   webDevelopers,
   webCoders,
-  hireDevelopers
+  hireDevelopers,
+  hireJavascriptDevelopers
 ]
 
 export default variants
