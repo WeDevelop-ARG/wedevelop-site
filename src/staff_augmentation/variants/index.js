5f7472a9010cc274533c73952d15726d258bf8ed
Abel M. Osorio <abel.m.osorio@gmail.com>
2021-10-06 10:49:51 -0300

    New variant web-programmers.

--- src/staff_augmentation/variants/index.js
+++ src/staff_augmentation/variants/index.js
@@ -1,6 +1,7 @@
 import staffAugmentation from './staff_augmentation'
 import webDevelopers from './web_developers'
 import webCoders from './web_coders'
+import webProgrammers from './web_programmers'
 import hireDevelopers from './hire_developers'
 import hireJavascriptDevelopers from './hire_javascript_developers'
 
@@ -8,6 +9,7 @@ const variants = [
   staffAugmentation,
   webDevelopers,
   webCoders,
+  webProgrammers,
   hireDevelopers,
   hireJavascriptDevelopers
 ]
