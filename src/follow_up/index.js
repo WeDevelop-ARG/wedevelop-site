651f6618901c97b5a920937d11e3d75b270fd682
Hiram J. González <hiram.gonzalez@wedevelop.me>
2021-10-07 13:19:38 -0400

    Implement Multi Step component in Follow Up page

--- src/follow_up/index.js
+++ src/follow_up/index.js
@@ -1,7 +1,6 @@
-import StepWizard from 'react-step-wizard'
-
 import Navbar from './components/Navbar'
 import Intro from './components/Intro'
+import MultiForm from './components/MultiForm'
 import Footer from './components/Footer'
 
 import FollowUpBackground from 'assets/follow_up/background.png'
@@ -17,21 +16,7 @@ function FollowUp () {
       <section className={classes.followUpContainer}>
         <div className={classes.stepsContainer}>
           <Intro />
-          <StepWizard>
-            <div>Step 1</div>
-            <div>Step 2</div>
-            <div>Step 3</div>
-            <div>Step 4</div>
-            <div>Step 5</div>
-            <div>Step 6</div>
-            <div>Step 7</div>
-            <div>Step 8</div>
-            <div>Step 9</div>
-            <div>Step 10</div>
-            <div>Step 11</div>
-            <div>Step 12</div>
-            <div>Step 13</div>
-          </StepWizard>
+          <MultiForm />
         </div>
         <img src={FollowUpMobileBackground} alt='' className={classes.mobileBackground} aria-hidden />
         <Footer />
