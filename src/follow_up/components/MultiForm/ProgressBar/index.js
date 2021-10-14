fd8770121beaf774477846e50e3cfeb371d5948e
Hiram J. González <hiram.gonzalez@wedevelop.me>
2021-10-14 12:15:10 -0400

    Implement useMemo  to handle Progress Bar changes

--- src/follow_up/components/MultiForm/ProgressBar/index.js
+++ src/follow_up/components/MultiForm/ProgressBar/index.js
@@ -1,14 +1,16 @@
+import { useMemo } from 'react'
+
 import ProgressBar from 'react-bootstrap/ProgressBar'
 
 import classes from './styles.module.scss'
 
 function FormProgress ({ currentStep, totalSteps }) {
-  const progressPercentage = () => {
+  const progressPercentage = useMemo(() => {
     return Math.round((currentStep * 100) / totalSteps)
-  }
+  }, [currentStep, totalSteps])
 
   return (
-    <ProgressBar now={progressPercentage()} className={classes.progress} />
+    <ProgressBar now={progressPercentage} className={classes.progress} />
   )
 }
 
