de8a1e5bfe1e602341b40d58a395701c2359131e
vwinnik <winnikvanina@gmail.com>
2021-10-15 13:03:13 -0300

    Implement RadioButtonIconCards component

---
+++ src/main_app/components/RadioButtonIconCards/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import Image from 'main_app/components/Image'
+import RadioButtonCard from '../RadioButtonCard'
+
+import classes from './styles.module.scss'
+
+function RadioButtonIconCards ({ options }) {
+  return (
+    <ul className={classes.listCards}>
+      {options.map(option => (
+        <li key={option.value}>
+          <RadioButtonCard
+            name='RadioButtonTextCards'
+            value={option.value}
+            className={classes.card}
+          >
+            <Image src={option.urlIcon} alt='' className={classes.urlIcon} />
+            <h3 className={classes.textCard}>{option.label}</h3>
+          </RadioButtonCard>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default RadioButtonIconCards
