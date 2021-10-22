346072732520cddb60cf7eab5b7d2aeee0386275
Hiram J. González <hiram.gonzalez@wedevelop.me>
2021-10-22 12:29:20 -0400

    Refactor MultiForm component

--- src/follow_up/components/MultiForm/index.js
+++ src/follow_up/components/MultiForm/index.js
@@ -1,9 +1,6 @@
-import { useState, useMemo } from 'react'
-import isUndefined from 'lodash/isUndefined'
 import StepWizard from 'react-step-wizard'
 
 import FormProgress from './ProgressBar'
-import ControlButtons from './ControlButtons'
 import ServiceType from './Steps/ServiceType'
 import ItProfessionals from './Steps/ItProfessionals'
 import Duration from './Steps/Duration'
@@ -15,25 +12,12 @@ import ScheduleCall from './Steps/ScheduleCall'
 import classes from './styles.module.scss'
 
 function MultiForm ({ handleModalOpen }) {
-  const [stepWizardInstance, setStepWizardInstance] = useState()
-  const [currentStep, setCurrentStep] = useState()
-
-  const fullCurrentStep = useMemo(() => {
-    if (!stepWizardInstance) return undefined
-    if (isUndefined(currentStep)) return stepWizardInstance.currentStep
-
-    return currentStep
-  }, [stepWizardInstance, currentStep])
-
   return (
     <>
       <StepWizard
-        initialStep={1}
-        onStepChange={({ activeStep }) => setCurrentStep(activeStep)}
         isLazyMount
         isHashEnabled
         nav={<FormProgress />}
-        instance={setStepWizardInstance}
         className={classes.stepWizardContainer}
       >
         <ServiceType hashKey='service-type' />
@@ -42,14 +26,11 @@ function MultiForm ({ handleModalOpen }) {
         <Budget hashKey='budget' />
         <StartingDate hashKey='starting-date' />
         <Technologies hashKey='technologies' />
-        <ScheduleCall hashKey='schedule-call' />
+        <ScheduleCall
+          hashKey='schedule-call'
+          skipModal={handleModalOpen}
+        />
       </StepWizard>
-      {stepWizardInstance &&
-        <ControlButtons
-          SW={stepWizardInstance}
-          currentStep={fullCurrentStep}
-          handleModalOpen={handleModalOpen}
-        />}
     </>
   )
 }
