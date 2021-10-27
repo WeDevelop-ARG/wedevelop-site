43701755db5e2a79d470a82328cf6721616058a7
vwinnik <winnikvanina@gmail.com>
2021-10-27 14:55:34 -0300

    Add PortfolioProjectCard component to portfolio page

--- src/portfolio/index.js
+++ src/portfolio/index.js
@@ -5,10 +5,13 @@ import NavBar from 'main_app/components/NavBar'
 import Article from 'main_app/components/Article'
 import ContactModal from 'main_app/components/ContactModal'
 import Footer from 'main_app/components/Footer'
+import PortfolioProjectCard from 'portfolio/components/PortfolioProjectCard'
+import useProjectCards from './components/PortfolioProjectCard/hooks/useProjectCards'
 
 import classes from './styles.module.scss'
 
 function Portfolio () {
+  const projectCards = useProjectCards()
   const contactPagePath = '/portfolio/contact'
   const match = useRouteMatch(contactPagePath)
   const history = useHistory()
@@ -29,6 +32,33 @@ function Portfolio () {
             <p className={classes.subheadingText}>success stories</p>
             <h2 className={classes.titleText}>Our Portfolio</h2>
             <hr className={classes.horizontalBar} />
+          </div>
+          <div className={classes.projectCards}>
+            {projectCards.map(({
+              id,
+              projectName,
+              coverImageURL,
+              logoURL,
+              logoBackgroundColor,
+              shortDescription,
+              tags,
+              description,
+              externalWebsiteURL,
+              detailsPagePath
+            }) => (
+              <PortfolioProjectCard
+                key={id}
+                projectName={projectName}
+                coverImageURL={coverImageURL}
+                logoURL={logoURL}
+                logoBackgroundColor={logoBackgroundColor}
+                shortDescription={shortDescription}
+                tags={tags}
+                description={description}
+                externalWebsiteURL={externalWebsiteURL}
+                detailsPagePath={detailsPagePath}
+              />
+            ))}
           </div>
         </section>
       </Article>
