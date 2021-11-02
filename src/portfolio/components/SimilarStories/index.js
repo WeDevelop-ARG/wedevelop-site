9eb33178cbad9077e92f53861c054ab3d48c5164
vwinnik <winnikvanina@gmail.com>
2021-11-02 12:59:55 -0300

    Add background and modify SimilarStories styles

--- src/portfolio/components/SimilarStories/index.js
+++ src/portfolio/components/SimilarStories/index.js
@@ -3,8 +3,10 @@ import { HashLink } from 'react-router-hash-link'
 import PortfolioProjectCard from 'portfolio/components/PortfolioProjectCard'
 import useProjectCards from '../PortfolioProjectCard/hooks/useProjectCards'
 import Image from 'main_app/components/Image'
+import BackgroundContainer from 'staff_augmentation/components/BackgroundContainer'
 
 import Arrow from 'assets/portfolio/arrow.svg'
+import similarStories from 'assets/portfolio/similarStories_bg.png'
 
 import classes from './styles.module.scss'
 
@@ -14,25 +16,28 @@ function SimilarStories () {
   const projectCards = useProjectCards().filter(projectCard => projectCard.externalWebsiteURL !== websiteURL)
 
   return (
-    <section id='stories' className={classes.sectionStories}>
-      <div>
-        <p className={classes.subheadingText}>success stories</p>
-        <h2 className={classes.titleText}>Similar stories to read</h2>
-        <hr className={classes.horizontalBar} />
-        <HashLink smooth to={projectCards.detailsPagePath} className={classes.viewAll}>
-          View All
-          <Image src={Arrow} alt='' className={classes.arrow} />
-        </HashLink>
-      </div>
-      <div className={classes.projectCards}>
-        {projectCards.slice(0, n).map(({ id, ...props }) => (
-          <PortfolioProjectCard
-            key={id}
-            {...props}
-          />
-        ))}
-      </div>
-    </section>
+    <>
+      <section id='stories'>
+        <div>
+          <p className={classes.subheadingText}>success stories</p>
+          <h2 className={classes.titleText}>Similar stories to read</h2>
+          <hr className={classes.horizontalBar} />
+          <HashLink smooth to={projectCards.detailsPagePath} className={classes.viewAll}>
+            View All
+            <Image src={Arrow} alt='' className={classes.arrow} />
+          </HashLink>
+        </div>
+        <div className={classes.projectCards}>
+          {projectCards.slice(0, n).map(({ id, ...props }) => (
+            <PortfolioProjectCard
+              key={id}
+              {...props}
+            />
+          ))}
+        </div>
+      </section>
+      <BackgroundContainer backgroundURL={similarStories} />
+    </>
   )
 }
 
