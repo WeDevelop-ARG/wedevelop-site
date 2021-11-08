5b6fd7837e240a3a625d3beda55619138250247c
Hiram J. González <hiram.gonzalez@wedevelop.me>
2021-11-08 19:46:51 -0400

    Add Clutch Widget to Story Details page

--- src/portfolio/story_details/index.js
+++ src/portfolio/story_details/index.js
@@ -4,6 +4,7 @@ import { useHistory, useRouteMatch } from 'react-router-dom'
 import useStoryDetailVariantByName from './hooks/useStoryDetailVariantByName'
 
 import Article from 'main_app/components/Article'
+import ClutchWidget from 'main_app/components/ClutchWidget'
 import ContactModal from 'main_app/components/ContactModal'
 import Footer from 'main_app/components/Footer'
 import GetInTouch from 'main_app/components/GetInTouch'
@@ -14,6 +15,8 @@ import ScrollTopButton from './components/ScrollTopButton'
 import Testimonials from 'main_app/components/Testimonials'
 import TestimonialsDecoration from './components/TestimonialsDecoration'
 import TextImageGeneric from './components/TextImageGeneric'
+
+import classes from './styles.module.scss'
 
 function StoryDetails () {
   const { params } = useRouteMatch('/portfolio/:name')
@@ -54,6 +57,12 @@ function StoryDetails () {
           customDecorations={<TestimonialsDecoration />}
           hideHeader
         />
+        <section className={classes.clutchContainer}>
+          <ClutchWidget
+            variant='dark'
+            className={classes.clutchWidget}
+          />
+        </section>
         <TextImageGeneric
           title={storyDetails.results.title}
           content={storyDetails.results.content}
