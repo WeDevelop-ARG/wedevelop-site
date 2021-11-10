d2d05f1fbc87b3c4cf12da9300dbbfdf8f6e250f
Hiram J. González <hiram.gonzalez@wedevelop.me>
2021-11-10 14:47:50 -0400

    Move How Does It Works section to Main App folder

---
+++ src/main_app/components/HowDoesItWorks/components/Step.js
@@ -0,0 +1,17 @@
+import classNames from 'classnames'
+
+import Image from 'main_app/components/Image'
+
+import classes from './styles.module.scss'
+
+function Step ({ className, icon, title, description }) {
+  return (
+    <div className={classNames(classes.column, className)}>
+      <Image src={icon} alt='' />
+      <h3 className={classes.title}>{title}</h3>
+      <p className={classes.description}>{description}</p>
+    </div>
+  )
+}
+
+export default Step
---
+++ src/main_app/components/HowDoesItWorks/index.js
@@ -0,0 +1,42 @@
+import classNames from 'classnames'
+
+import Image from '../Image'
+import Step from './components/Step'
+
+import { useStep } from './components/hooks/useStep'
+
+import CurvedArrow from 'assets/how_does_it_works/arrows/curved_arrow.svg'
+
+import classes from './styles.module.scss'
+
+function HowDoesItWorks ({ className }) {
+  const steps = useStep()
+  return (
+    <section className={classNames(classes.container, className)}>
+      <div className={classes.sectionHeader}>
+        <p className={classes.subheading}>Screening Process</p>
+        <h2 className={classes.title}>How does it work?</h2>
+        <hr className={classes.horizontalBar} />
+      </div>
+      <div className={classes.stepsContainer}>
+        <div className={classes.remarkContainer}>
+          <p className={classes.remark}>We’ll send you  more profiles on a daily basis</p>
+          <Image src={CurvedArrow} alt='' className={classes.curvedArrow} />
+        </div>
+        <div className={classes.row}>
+          {steps && steps.map(({ id, icon, title, description, arrow }) =>
+            <Step
+              key={id}
+              icon={icon}
+              title={title}
+              description={description}
+              className={classes.step}
+            />
+          )}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default HowDoesItWorks
