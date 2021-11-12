07796e55586a56e6ae9462fb5fd2298c123cf5ad
Hiram J. González <hiram.gonzalez@wedevelop.me>
2021-11-12 10:51:36 -0400

    Improve How Does It Works styles and responsiveness

--- src/main_app/components/HowDoesItWorks/components/Step.js
+++ src/main_app/components/HowDoesItWorks/components/Step.js
@@ -7,7 +7,7 @@ import classes from './styles.module.scss'
 function Step ({ className, icon, title, description }) {
   return (
     <div className={classNames(classes.column, className)}>
-      <Image src={icon} alt='' />
+      <Image src={icon} alt='' className={classes.icon} />
       <h3 className={classes.title}>{title}</h3>
       <p className={classes.description}>{description}</p>
     </div>
--- src/main_app/components/HowDoesItWorks/index.js
+++ src/main_app/components/HowDoesItWorks/index.js
@@ -6,6 +6,7 @@ import Step from './components/Step'
 import { useStep } from './components/hooks/useStep'
 
 import CurvedArrow from 'assets/how_does_it_works/arrows/curved_arrow.svg'
+import DesktopSteps from 'assets/how_does_it_works/steps.svg'
 
 import classes from './styles.module.scss'
 
@@ -23,15 +24,25 @@ function HowDoesItWorks ({ className }) {
           <p className={classes.remark}>We’ll send you  more profiles on a daily basis</p>
           <Image src={CurvedArrow} alt='' className={classes.curvedArrow} />
         </div>
+        <Image src={DesktopSteps} alt='' className={classes.desktopSteps} />
         <div className={classes.row}>
           {steps && steps.map(({ id, icon, title, description, arrow }) =>
-            <Step
-              key={id}
-              icon={icon}
-              title={title}
-              description={description}
-              className={classes.step}
-            />
+            (
+              <>
+                <Step
+                  key={id}
+                  icon={icon}
+                  title={title}
+                  description={description}
+                  className={classes.step}
+                />
+                <Image
+                  key={id}
+                  src={arrow}
+                  alt=''
+                  className={classes.arrows} />
+              </>
+            )
           )}
         </div>
       </div>
