1d3eb288fa829f80ee83d2ed949976c27607a9e6
Hiram J. González <hiram.gonzalez@wedevelop.me>
2021-11-25 14:22:49 -0400

    Implement new Free Quote Form

--- src/staff_augmentation/components/FreeQuoteForm/index.js
+++ src/staff_augmentation/components/FreeQuoteForm/index.js
@@ -1,43 +1,18 @@
-import { Fragment, useCallback } from 'react'
-import classnames from 'classnames'
-import { Field } from 'formik'
+import { useCallback } from 'react'
 import { useHistory } from 'react-router'
 
-import SubmitButton from 'main_app/components/SubmitButton'
-import Textarea from 'main_app/components/Textarea'
-import ReCAPTCHAField from 'main_app/components/ReCAPTCHAField'
-import useFieldWithErrorClassName from 'utils/use_field_with_error_class_name'
-
-import FormLogic from './FormLogic'
+import HubspotFreeQuoteForm from '../HubspotFreeQuoteForm'
 
 import classes from './styles.module.scss'
 
 function FreeQuoteForm ({
   formHeader,
-  fixedFields,
-  formOrigin,
-  formButtonText,
-  formDisclaimer,
-  noAutofocus
+  formOrigin
 }) {
   const history = useHistory()
   const handleSubmitFinished = useCallback(tracingId => {
     history.push('/follow-up?tracingId=' + tracingId)
   }, [history])
-  const TextAreaWithError = useFieldWithErrorClassName(
-    Textarea,
-    classes.fieldWithError
-  )
-  const InputWithError = useFieldWithErrorClassName(
-    'input',
-    classes.fieldWithError
-  )
-  const initialValues = {
-    name: '',
-    email: '',
-    message: '',
-    recaptchaToken: ''
-  }
 
   return (
     <>
@@ -47,48 +22,10 @@ function FreeQuoteForm ({
         <p className={classes.descriptionText}>{formHeader.description}</p>
         <hr className={classes.horizontalBar} />
       </div>
-      <FormLogic
-        initialValues={initialValues}
+      <HubspotFreeQuoteForm
         formOrigin={formOrigin}
-        onSubmitFinished={handleSubmitFinished}
-        className={classes.form}
-      >
-        <Field
-          as={InputWithError}
-          type='text'
-          name='name'
-          placeholder={fixedFields.name.placeholder}
-          className={classes.inputStyles}
-          autoFocus={noAutofocus !== true}
-        />
-        <Field
-          as={InputWithError}
-          type='email'
-          name='email'
-          placeholder={fixedFields.email.placeholder}
-          className={classes.inputStyles}
-        />
-        <label className={classes.fieldLabel}>{fixedFields.message.label}</label>
-        <Field
-          as={TextAreaWithError}
-          name='message'
-          placeholder={fixedFields.message.placeholder}
-          maxLength='200'
-          className={classnames(classes.inputStyles, classes.textarea)}
-        />
-        <ReCAPTCHAField name='recaptchaToken' className={classes.recaptcha} />
-        <div className={classes.buttonContainer}>
-          <SubmitButton
-            variant='primary'
-            className={classes.buttonStyles}
-          >
-            {formButtonText}
-          </SubmitButton>
-        </div>
-        <div className={classes.formDisclaimer}>
-          <p>{formDisclaimer}</p>
-        </div>
-      </FormLogic>
+        handleSubmitFinished={handleSubmitFinished}
+      />
     </>
   )
 }
