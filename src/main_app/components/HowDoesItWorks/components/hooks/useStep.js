4e43b32345e1ba87850dd2b14a4f71670177572f
Hiram J. González <hiram.gonzalez@wedevelop.me>
2021-11-29 17:40:32 -0400

    Fix styling issues in How Does It Works section

--- src/main_app/components/HowDoesItWorks/components/hooks/useStep.js
+++ src/main_app/components/HowDoesItWorks/components/hooks/useStep.js
@@ -2,29 +2,25 @@ import ChecklistIcon from 'assets/how_does_it_works/icons/checklist_icon.svg'
 import SelectionIcon from 'assets/how_does_it_works/icons/selection_icon.svg'
 import InterviewsIcon from 'assets/how_does_it_works/icons/interviews_icon.svg'
 import OnboardingIcon from 'assets/how_does_it_works/icons/onboarding_icon.svg'
-import Arrow from 'assets/how_does_it_works/arrows/horizontal_arrow.svg'
 
 const steps = [
   {
     id: 1,
     icon: ChecklistIcon,
     title: 'Requirements',
-    description: 'Fill out a form and let us know your needs.',
-    arrow: Arrow
+    description: 'Fill out a form and let us know your needs.'
   },
   {
     id: 2,
     icon: SelectionIcon,
     title: 'Selection & Screening',
-    description: 'We’ll get back to you within 1-2 hours to refine the Job description and present you some profiles from our Talent Pool.',
-    arrow: Arrow
+    description: 'We’ll get back to you within 1-2 hours to refine the Job description and present you some profiles from our Talent Pool.'
   },
   {
     id: 3,
     icon: InterviewsIcon,
     title: 'Interviews with the client',
-    description: 'You’ll evaluate these candidates and interview the ones that you like the most.',
-    arrow: Arrow
+    description: 'You’ll evaluate these candidates and interview the ones that you like the most.'
   },
   {
     id: 4,
