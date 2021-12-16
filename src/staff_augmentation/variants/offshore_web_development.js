e5765e7a3f377a2b154781290fc1ff73b8a8682c
Ariel <ariel.pacciaroni@wedevelop.me>
2021-12-16 10:54:52 -0300

    Added offshore webdevelopment landing

---
+++ src/staff_augmentation/variants/offshore_web_development.js
@@ -0,0 +1,35 @@
+import BgWebDevelopers from 'assets/web_developers/backgrounds/bg_web_developers.png'
+
+import defaultContent from './default_content'
+
+const offshoreWebDevelopment = {
+  ...defaultContent,
+  pathName: 'offshore-web-development',
+  metadata: {
+    title: 'Offshore Web Development Agency',
+    description: 'Outsource with Us and Accelerate your Digital Transformation.'
+  },
+  header: {
+    landingName: <>offshore web development</>,
+    title: <>Top notch <b>Offshore Web Development</b></>,
+    description: (
+      <>
+        <p>Hire Offshore Web Development without the hassles.</p>
+      </>
+    ),
+    sideImageURL: BgWebDevelopers,
+    backgroundColor: '#361989'
+  },
+  freeQuoteForm: {
+    ...defaultContent.freeQuoteForm,
+    formHeader: {
+      subtitle: 'Get in touch',
+      title: 'Get Offshore Web Development profiles',
+      description: 'Fill out the form and receive a list of vetted Web Developers.'
+    },
+    formOrigin: 'web-developers',
+    formButtonText: 'Get Offshore Web Development profiles'
+  }
+}
+
+export default offshoreWebDevelopment
