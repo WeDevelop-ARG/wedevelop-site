8ea2815e531cb3c6030b8c7ae900f67c2afdcfe2
Hiram J. González <hiram.gonzalez@wedevelop.me>
2022-01-14 13:23:33 -0400

    Modify File Input component

--- src/career/components/JoinUsForm/FileInput/index.js
+++ src/career/components/JoinUsForm/FileInput/index.js
@@ -1,22 +1,30 @@
-import { isEmpty } from 'lodash'
+import { useCallback } from 'react'
+import { isNil } from 'lodash'
 import SuccessUploaded from './SuccessUploaded'
 
-function FileInput ({ form, field }) {
+function FileInput ({ form, field, children }) {
   const handleChange = (event) => {
     const file = event.currentTarget.files[0]
     form.setFieldTouched(field.name)
     form.setFieldValue(field.name, file, true)
   }
 
-  if (!isEmpty(field.value)) return <SuccessUploaded file={field.value} />
-  
+  const handleDeleteFile = useCallback(() => {
+    form.setFieldValue(field.name, undefined, true)
+  }, [form, field])
+
+  if (!isNil(field.value)) return <SuccessUploaded fileName={field.value.name} fileSize={field.value.size} handleDeleteFile={handleDeleteFile} />
+
   return (
-    <input
-      type='file'
-      onChange={(input) => handleChange(input)}
-      style={{ display: 'none' }}
-      aria-hidden='true'
-    />
+    <>
+      <input
+        type='file'
+        onChange={(input) => handleChange(input)}
+        style={{ display: 'none' }}
+        aria-hidden='true'
+      />
+      {children}
+    </>
   )
 }
 
