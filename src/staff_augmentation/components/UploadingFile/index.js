92b79625f211c3b281e117f72d05092c8ac859bd
Hiram J. González <hiram.gonzalez@wedevelop.me>
2022-01-14 13:27:36 -0400

    Add Progress Bar to Uploading File component

--- src/staff_augmentation/components/UploadingFile/index.js
+++ src/staff_augmentation/components/UploadingFile/index.js
@@ -1,22 +1,22 @@
-import { Field } from 'formik'
-import FileInput from 'career/components/JoinUsForm/FileInput'
+import { useMemo } from 'react'
+import ProgressBar from 'react-bootstrap/ProgressBar'
+
 import SVGIcon from 'main_app/components/SVGIcon'
+
 import classes from './styles.module.scss'
 
-export default function UploadingFile() {
-  
+export default function UploadingFile ({ currentProgress, fileName }) {
+  const progressPercentage = useMemo(() => {
+    return Math.round(currentProgress * 100)
+  }, [currentProgress])
+
   return (
-  <label className={classes.fileUploadField}>
-    <Field
-      name='filesAttached'
-      component={FileInput}
-      aria-hidden='true'
-    />
+    <label className={classes.fileUploadField}>
       <div className={classes.field}>
         <SVGIcon name='career/uploadingFile' />
         <p className={classes.fileUploadLabel}>Nombre del archivo</p>
       </div>
-  </label>
+      <ProgressBar now={progressPercentage} className={classes.progress} />
+    </label>
   )
-  //TODO: Agregar prop 'nombre del archivo' y barra de carga en progreso.
 }
