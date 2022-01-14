b12e73458551ac80eccaa60dab042164569f8d07
Hiram J. González <hiram.gonzalez@wedevelop.me>
2022-01-14 13:28:10 -0400

    Enable Try Again in Error Upload File component

--- src/staff_augmentation/components/ErrorUploadingFile/index.js
+++ src/staff_augmentation/components/ErrorUploadingFile/index.js
@@ -1,27 +1,19 @@
-import { Field } from 'formik'
 import SVGIcon from 'main_app/components/SVGIcon'
 import Button from 'main_app/components/Button'
-import FileInput from 'career/components/JoinUsForm/FileInput'
 
 import classes from './styles.module.scss'
 
-export default function ErrorUploadingFile() {
-  
+export default function ErrorUploadingFile ({ retryButtonProps }) {
   return (
-  <label className={classes.fileUploadField}>
-    <Field
-      name='filesAttached'
-      component={FileInput}
-      aria-hidden='true'
-    />
+    <label className={classes.fileUploadField}>
       <div className={classes.field}>
         <SVGIcon name='career/errorUploadingFile' />
         <p className={classes.fileUploadLabel}>Something went wrong trying to upload your file.</p>
-        <Button variant='link' className={classes.tryAgain} onClick={''}>
+        <Button variant='link' className={classes.tryAgain} {...retryButtonProps}>
           Try again
         </Button>
       </div>
-  </label>
+    </label>
   )
-  //TODO: Agregar props para Try again
+  // TODO: Agregar props para Try again
 }
