7b3fa164583fd94eb959dd150771568e3d23bd30
Vanina <winnikvanina@gmail.com>
2022-01-16 18:39:41 -0300

    Modify ErrorUploadingFile styles

--- src/staff_augmentation/components/ErrorUploadingFile/index.js
+++ src/staff_augmentation/components/ErrorUploadingFile/index.js
@@ -5,14 +5,20 @@ import classes from './styles.module.scss'
 
 export default function ErrorUploadingFile ({ retryButtonProps }) {
   return (
-    <label className={classes.fileUploadField}>
-      <div className={classes.field}>
-        <SVGIcon name='career/errorUploadingFile' alt='' className={classes.errorUploadingFile} />
-        <p className={classes.fileUploadLabel}>Something went wrong trying to upload your file.</p>
-        <Button variant='link' className={classes.tryAgain} {...retryButtonProps}>
-          Try again
-        </Button>
+    <div className={classes.field}>
+      <div className={classes.errorUploadingMessage}>
+        <SVGIcon
+          name='career/errorUploadingFile'
+          alt=''
+          className={classes.errorUploadingFile}
+        />
+        <p className={classes.fileUploadLabel}>
+          Something went wrong trying to upload your file.
+        </p>
       </div>
-    </label>
+      <Button variant='link' className={classes.tryAgain} {...retryButtonProps}>
+        Try again
+      </Button>
+    </div>
   )
 }
