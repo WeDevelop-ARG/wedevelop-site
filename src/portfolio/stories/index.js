4cee5249cfb26392b22124254e67e41b6d477f99
Hiram J. González <hiram.gonzalez@wedevelop.me>
2022-01-19 13:03:54 -0400

    Implement new stories in Portfolio

--- src/portfolio/stories/index.js
+++ src/portfolio/stories/index.js
@@ -1,17 +1,18 @@
-
 import ballwizDetails from './ballwiz_details'
 import copaDetails from './copa_details'
 import cvsdDetails from './cvsd_details'
+import drCannabisDetails from './drCannabis_details'
 import injobDetails from './injob_details'
 import monedaDetails from './moneda_details'
 import raftDetails from './raft_details'
-import drCannabisDetails from './drCannabis_details'
+import trainWithDetails from './trainWith_details'
 
 export const stories = [
-  ballwizDetails,
   copaDetails,
   cvsdDetails,
+  ballwizDetails,
   raftDetails,
+  trainWithDetails,
   injobDetails,
   monedaDetails,
   drCannabisDetails
