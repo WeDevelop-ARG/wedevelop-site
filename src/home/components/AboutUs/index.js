fd020216a73cd32d492a3dea2db11f915541d150
Nahuel Dallacamina <nahudalla@gmail.com>
2022-01-20 18:22:09 -0300

    Position images in About Us section of Home

--- src/home/components/AboutUs/index.js
+++ src/home/components/AboutUs/index.js
@@ -1,8 +1,8 @@
-import InternalLink from 'main_app/components/InternalLink'
-
-
-import Button from 'main_app/components/Button'
 import Image from 'next/image'
+
+import InternalLink from 'main_app/components/InternalLink'
+import Button from 'main_app/components/Button'
+import WrappedImage from 'main_app/components/WrappedImage'
 
 import SmilingFace from 'assets/home/smiling_face.svg'
 import CuriousHead from 'assets/home/curious_head.svg'
@@ -25,21 +25,21 @@ function AboutUs () {
         </div>
         <div className={classes.sectionContent}>
           <div className={classes.aboutUsBullet}>
-            <Image src={SmilingFace} alt='' className={classes.bulletImg} />
+            <WrappedImage src={SmilingFace} alt='' className={classes.bulletImg} />
             <p>
               <b>We love what we do</b> - Our indomitable spirit makes
               us live our dreams by doing what we love.
             </p>
           </div>
           <div className={classes.aboutUsBullet}>
-            <Image src={CuriousHead} alt='' className={classes.bulletImg} />
+            <WrappedImage src={CuriousHead} alt='' className={classes.bulletImg} />
             <p>
               <b>We are curious</b> - On an exponential growing world,
               we stay on the edge. And we love it!
             </p>
           </div>
           <div className={classes.aboutUsBullet}>
-            <Image src={DreamTeam} alt='' className={classes.bulletImg} />
+            <WrappedImage src={DreamTeam} alt='' className={classes.bulletImg} />
             <p>
               <b>We work as a Dream Team</b> - We have fun and learn
               from each other. Everyone is a key player.
@@ -49,10 +49,10 @@ function AboutUs () {
         <Button
           as={InternalLink}
           href='/contact'
-          smooth
           isAnchor
           variant='primary'
           className={classes.moreInfoButton}
+          link={{ scroll: false }}
         >
           More Info
         </Button>
@@ -61,7 +61,12 @@ function AboutUs () {
         <div className={classes.smallCircle} aria-hidden='true' />
         <div className={classes.smallBlurCircle} aria-hidden='true' />
       </section>
-      <Image src={BottomImg} alt='' className={classes.bottomImg} />
+      <WrappedImage
+        src={BottomImg}
+        alt=''
+        layout='fill'
+        className={classes.bottomImg}
+      />
     </>
   )
 }
