3c720ef77e189daa2590e00620f863ee8ab6ef6d
Hiram J. González <hiram.gonzalez@wedevelop.me>
2022-01-26 12:38:52 -0400

    Fix background and positioning for Tech Stack Icons component

--- src/portfolio/components/TechStackIcons/index.js
+++ src/portfolio/components/TechStackIcons/index.js
@@ -1,17 +1,20 @@
 import Image from 'main_app/components/Image'
+
 import classes from './styles.module.scss'
 
-function TechStackIcons({ icons }) {
+function TechStackIcons ({ icons }) {
   return (
-    <section className={classes.container}>
-      <h3 className={classes.title}>Tech Stack</h3>
-      <div className={classes.listSkill} align='center'>
-        {icons.map(({ id, name, photo }) => (
-          <div className={classes.skill} key={id}>
-            <Image src={photo} alt='' className={classes.logoSkill} />
-            <p className={classes.name}>{name}</p>
-          </div>
-        ))}
+    <section className={classes.techStackSection}>
+      <div className={classes.content}>
+        <h3 className={classes.title}>Tech Stack</h3>
+        <div className={classes.listSkill} align='center'>
+          {icons.map(({ id, name, photo }) => (
+            <figure className={classes.skill} key={id}>
+              <Image src={photo} alt='' className={classes.logoSkill} />
+              <figcaption className={classes.name}>{name}</figcaption>
+            </figure>
+          ))}
+        </div>
       </div>
     </section>
   )
