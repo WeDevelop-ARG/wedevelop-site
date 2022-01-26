a31bec0cf508f2270ad43579bf6b00a44d8bc59f
Vanina <vaniwinnik@gmail.com>
2022-01-26 14:23:10 -0300

    Add HashLink to PortfolioProjectCard component

--- src/portfolio/components/PortfolioProjectCard/index.js
+++ src/portfolio/components/PortfolioProjectCard/index.js
@@ -34,61 +34,61 @@ function PortfolioProjectCard ({
   const index = description.substring(0, cantidadCaracteres).lastIndexOf(' ')
   const newDescription = description.substring(0, index).concat('...')
   return (
-    <div
-      className={classes.container}
-      onMouseOver={() => setIsOnHover(true)}
-      onMouseOut={() => setIsOnHover(false)}
-    >
-      <Image
-        src={coverImageURL}
-        alt={shortDescription}
-        objectFit='cover'
-        position='bottom'
-        placeholderColor='#FFF'
-        className={classes.headerImage}
-      />
-      <ProjectLogo
-        logoURL={logoURL}
-        projectName={projectName}
-        className={classes.logo}
-        {...logoStyleProp}
-      />
-      <div className={classes.projectDetails}>
-        <div className={classes.header}>
-          <h3 className={classes.projectName}>{projectName}</h3>
-          {externalWebsiteURL && (
-            <a
-              href={externalWebsiteURL}
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <World role='presentation' className={classes.world} />
-            </a>
-          )}
+    <HashLink to={detailsPagePath} smooth
+        className={classes.container}
+        onMouseOver={() => setIsOnHover(true)}
+        onMouseOut={() => setIsOnHover(false)}
+      >
+        <Image
+          src={coverImageURL}
+          alt={shortDescription}
+          objectFit='cover'
+          position='bottom'
+          placeholderColor='#FFF'
+          className={classes.headerImage}
+        />
+        <ProjectLogo
+          logoURL={logoURL}
+          projectName={projectName}
+          className={classes.logo}
+          {...logoStyleProp}
+        />
+        <div className={classes.projectDetails}>
+          <div className={classes.header}>
+            <h3 className={classes.projectName}>{projectName}</h3>
+            {externalWebsiteURL && (
+              <a
+                href={externalWebsiteURL}
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <World role='presentation' className={classes.world} />
+              </a>
+            )}
+          </div>
+          <p className={classes.shortDescription}>{shortDescription}</p>
+          <div className={classes.tagsContainer}>
+            {tags.map((tag) => (
+              <span className={classes.tag} key={tag}>
+                {tag}
+              </span>
+            ))}
+          </div>
+          <p className={classes.description}>{newDescription}</p>
+          <Button
+            as={HashLink}
+            to={detailsPagePath}
+            smooth
+            variant={isOnHover ? 'primary' : 'link'}
+            iconRight={isOnHover ? <WhiteArrow /> : <BlueArrow />}
+            className={classNames(classes.readMore, {
+              [classes.readMoreWhite]: isOnHover
+            })}
+          >
+            Read More
+          </Button>
         </div>
-        <p className={classes.shortDescription}>{shortDescription}</p>
-        <div className={classes.tagsContainer}>
-          {tags.map((tag) => (
-            <span className={classes.tag} key={tag}>
-              {tag}
-            </span>
-          ))}
-        </div>
-        <p className={classes.description}>{newDescription}</p>
-        <Button
-          as={HashLink}
-          to={detailsPagePath}
-          smooth
-          variant={isOnHover ? 'primary' : 'link'}
-          iconRight={isOnHover ? <WhiteArrow /> : <BlueArrow />}
-          className={classNames(classes.readMore, {
-            [classes.readMoreWhite]: isOnHover
-          })}
-        >
-          Read More
-        </Button>
-      </div>
-    </div>
+    </HashLink>
   )
 }
 
