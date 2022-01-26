1877585c89b56834b33cedfead7eeb1edb4763f6
Vanina <vaniwinnik@gmail.com>
2022-01-26 15:52:34 -0300

    Add useMediaQuery hook

--- src/portfolio/components/PortfolioProjectCard/index.js
+++ src/portfolio/components/PortfolioProjectCard/index.js
@@ -5,6 +5,8 @@ import classNames from 'classnames'
 import Button from 'main_app/components/Button'
 import Image from 'main_app/components/Image'
 import ProjectLogo from '../ProjectLogo'
+import useMediaQuery from 'utils/use_media_query'
+import { forPhoneOnly } from 'styles/media_queries'
 
 import { ReactComponent as World } from 'assets/portfolio/world.svg'
 import { ReactComponent as BlueArrow } from 'assets/portfolio/arrow.svg'
@@ -12,7 +14,7 @@ import { ReactComponent as WhiteArrow } from 'assets/portfolio/white_horizontal_
 
 import classes from './styles.module.scss'
 
-function PortfolioProjectCard ({
+function PortfolioProjectCard({
   coverImageURL,
   logoURL,
   logoBackground,
@@ -30,64 +32,72 @@ function PortfolioProjectCard ({
     logoStyleProp.style = { backgroundImage: logoBackground }
   }
   const [isOnHover, setIsOnHover] = useState(false)
+  const isPhoneOnly = useMediaQuery(forPhoneOnly)
   const cantidadCaracteres = 145
   const index = description.substring(0, cantidadCaracteres).lastIndexOf(' ')
   const newDescription = description.substring(0, index).concat('...')
+
   return (
-    <HashLink to={detailsPagePath} smooth
-        className={classes.container}
-        onMouseOver={() => setIsOnHover(true)}
-        onMouseOut={() => setIsOnHover(false)}
-      >
-        <Image
-          src={coverImageURL}
-          alt={shortDescription}
-          objectFit='cover'
-          position='bottom'
-          placeholderColor='#FFF'
-          className={classes.headerImage}
-        />
-        <ProjectLogo
-          logoURL={logoURL}
-          projectName={projectName}
-          className={classes.logo}
-          {...logoStyleProp}
-        />
-        <div className={classes.projectDetails}>
-          <div className={classes.header}>
-            <h3 className={classes.projectName}>{projectName}</h3>
-            {externalWebsiteURL && (
-              <a
-                href={externalWebsiteURL}
-                target='_blank'
-                rel='noopener noreferrer'
-              >
-                <World role='presentation' className={classes.world} />
-              </a>
-            )}
-          </div>
-          <p className={classes.shortDescription}>{shortDescription}</p>
-          <div className={classes.tagsContainer}>
-            {tags.map((tag) => (
-              <span className={classes.tag} key={tag}>
-                {tag}
-              </span>
-            ))}
-          </div>
-          <p className={classes.description}>{newDescription}</p>
-          <Button
-            as={HashLink}
-            to={detailsPagePath}
-            smooth
-            variant={isOnHover ? 'primary' : 'link'}
-            iconRight={isOnHover ? <WhiteArrow /> : <BlueArrow />}
-            className={classNames(classes.readMore, {
-              [classes.readMoreWhite]: isOnHover
-            })}
-          >
-            Read More
-          </Button>
+    <HashLink
+      to={detailsPagePath}
+      smooth
+      className={classes.container}
+      onMouseOver={() => setIsOnHover(true)}
+      onMouseOut={() => setIsOnHover(false)}
+    >
+      <Image
+        src={coverImageURL}
+        alt={shortDescription}
+        objectFit='cover'
+        position='bottom'
+        placeholderColor='#FFF'
+        className={classes.headerImage}
+      />
+      <ProjectLogo
+        logoURL={logoURL}
+        projectName={projectName}
+        className={classes.logo}
+        {...logoStyleProp}
+      />
+      <div className={classes.projectDetails}>
+        <div className={classes.header}>
+          <h3 className={classes.projectName}>{projectName}</h3>
+          {externalWebsiteURL && (
+            <a
+              href={externalWebsiteURL}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <World role='presentation' className={classes.world} />
+            </a>
+          )}
         </div>
+        <p className={classes.shortDescription}>{shortDescription}</p>
+        {!isPhoneOnly && (
+          <>
+            <div className={classes.tagsContainer}>
+              {tags.map((tag) => (
+                <span className={classes.tag} key={tag}>
+                  {tag}
+                </span>
+              ))}
+            </div>
+            <p className={classes.description}>{newDescription}</p>
+          </>
+        )}
+        <Button
+          as={HashLink}
+          to={detailsPagePath}
+          smooth
+          variant={isOnHover ? 'primary' : 'link'}
+          iconRight={isOnHover ? <WhiteArrow /> : <BlueArrow />}
+          className={classNames(classes.readMore, {
+            [classes.readMoreWhite]: isOnHover
+          })}
+        >
+          Read More
+        </Button>
+      </div>
     </HashLink>
   )
 }
