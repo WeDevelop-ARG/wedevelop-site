99abf45c236c4854b0842f4efdbc259a5a3fe22e
Hiram J. González <hiram.gonzalez@wedevelop.me>
2022-01-27 14:41:22 -0400

    Workin on Story Header fixes

--- src/portfolio/components/StoryHeader/index.js
+++ src/portfolio/components/StoryHeader/index.js
@@ -1,7 +1,5 @@
 import { HashLink } from 'react-router-hash-link'
 
-import useMediaQuery from 'utils/use_media_query'
-
 import Button from 'main_app/components/Button'
 import Image from 'main_app/components/Image'
 
@@ -10,13 +8,10 @@ import DecoCircles from 'assets/portfolio/header_deco_circles.svg'
 import ProjectLogo from '../ProjectLogo'
 import WorldIcon from 'assets/portfolio/world.svg'
 
-import { forDesktopUp } from 'styles/media_queries'
-
 import classes from './styles.module.scss'
 
 function StoryHeader ({
   backgroundURL,
-  mobileBackgroundURL,
   logoURL,
   heading,
   title,
@@ -27,8 +22,6 @@ function StoryHeader ({
   imageURL,
   logoBackground
 }) {
-  const isDesktopUp = useMediaQuery(forDesktopUp)
-
   const logoStyleProp = {}
   if (logoBackground.startsWith('#')) {
     logoStyleProp.style = { backgroundColor: logoBackground }
@@ -36,7 +29,7 @@ function StoryHeader ({
     logoStyleProp.style = { backgroundImage: logoBackground }
   }
   return (
-    <section className={classes.headerContainer}>
+    <>
       <div className={classes.decorationWrapper}>
         <Button
           as={HashLink}
@@ -49,45 +42,48 @@ function StoryHeader ({
           Back to list
         </Button>
         <Image
-          src={isDesktopUp ? backgroundURL : mobileBackgroundURL}
+          src={backgroundURL}
           alt=''
           loading='eager'
           placeholderColor='#333'
+          layout='fill'
+          objectFit='cover'
+          position='bottom'
           className={classes.headerBackground}
         />
+        <ProjectLogo logoURL={logoURL} className={classes.projectLogo} {...logoStyleProp} />
       </div>
-      <div className={classes.content}>
-        <div className={classes.logoContainer}>
-          <ProjectLogo logoURL={logoURL} {...logoStyleProp} />
+      <section className={classes.headerContainer}>
+        <div className={classes.content}>
+          <p className={classes.heading}>{heading}</p>
+          <div className={classes.titleContainer}>
+            <h2 className={classes.title}>{title}</h2>
+            {websiteURL &&
+              <a
+                href={websiteURL}
+                target='_blank'
+                rel='noopener noreferrer'
+                className={classes.websiteLink}
+              >
+                Website
+                <Image src={WorldIcon} alt='' className={classes.worldIcon} />
+              </a>}
+          </div>
+          <hr className={classes.horizontalBar} />
+          <h3 className={classes.subtitle}>{subtitle}</h3>
+          <div className={classes.tagsContainer}>
+            {tags.map((tag) => (
+              <span key={tag} className={classes.tag}>
+                {tag}
+              </span>
+            ))}
+          </div>
+          <p className={classes.description}>{description}</p>
         </div>
-        <p className={classes.heading}>{heading}</p>
-        <div className={classes.titleContainer}>
-          <h2 className={classes.title}>{title}</h2>
-          {websiteURL &&
-            <a
-              href={websiteURL}
-              target='_blank'
-              rel='noopener noreferrer'
-              className={classes.websiteLink}
-            >
-              Website
-              <Image src={WorldIcon} alt='' className={classes.worldIcon} />
-            </a>}
-        </div>
-        <hr className={classes.horizontalBar} />
-        <h3 className={classes.subtitle}>{subtitle}</h3>
-        <div className={classes.tagsContainer}>
-          {tags.map((tag) => (
-            <span key={tag} className={classes.tag}>
-              {tag}
-            </span>
-          ))}
-        </div>
-        <p className={classes.description}>{description}</p>
-      </div>
-      <Image src={imageURL} alt='' className={classes.bottomImg} />
-      <Image src={DecoCircles} alt='' className={classes.decoCircles} />
-    </section>
+        <Image src={imageURL} alt='' className={classes.bottomImg} />
+        <Image src={DecoCircles} alt='' className={classes.decoCircles} />
+      </section>
+    </>
   )
 }
 
