977a0c54153a937c108d3f888966eb441dd18a67
Nahuel Dallacamina Ortea <nahuel@wedevelop.me>
2022-01-27 16:07:36 -0300

    Remove prop

--- src/main_app/components/InternalLink/index.jsx
+++ src/main_app/components/InternalLink/index.jsx
@@ -1,11 +1,11 @@
 import Link from 'next/link'
 
 export default function InternalLink (props) {
-  const { href, smooth, link = {}, ...rest } = props
+  const { href, link = {}, ...rest } = props
 
   return (
     <Link href={href} {...link}>
-      <a smooth={smooth ? 'true' : undefined} {...rest}>
+      <a {...rest}>
         {props.children}
       </a>
     </Link>
