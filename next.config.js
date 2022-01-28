a4431763e387f0edd11ec66c6bea3bc506d962f3
Nahuel Dallacamina <nahudalla@gmail.com>
2022-01-28 14:45:23 -0300

    Reduce CSS size by changing class names

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 const { ENVIRONMENT } = require('./src/main_app/constants')
 
-module.exports = {
+module.exports = require('next-optimized-classnames')({
   images: {
     loader: 'cloudinary',
-    path: `https://res.cloudinary.com/wedevelop-site/image/upload`
+    path: 'https://res.cloudinary.com/wedevelop-site/image/upload'
   },
-  assetPrefix: ENVIRONMENT !== 'development' ? '/assets-'+ENVIRONMENT : '',
-  webpack(config) {
+  assetPrefix: ENVIRONMENT !== 'development' ? '/assets-' + ENVIRONMENT : '',
+  webpack (config) {
     for (const rule of config.module.rules) {
       if (rule.test?.test('file.svg')) {
         rule.test = new RegExp('(?<!\\.component)' + rule.test.source, rule.test.flags)
@@ -25,4 +25,4 @@ module.exports = {
 
     return config
   }
-}
+})
