61a0bbe13c94d74f6043884be58d163ce7e72593
Hiram J. González <hiram.gonzalez@wedevelop.me>
2022-01-28 10:25:38 -0400

    Disable all landing pages except It Outsourcing

--- src/staff_augmentation/variants/index.js
+++ src/staff_augmentation/variants/index.js
@@ -1,41 +1,7 @@
-import staffAugmentation from './staff_augmentation'
-import webDevelopers from './web_developers'
-import webCoders from './web_coders'
-import webProgrammers from './web_programmers'
-import hireDevelopers from './hire_developers'
-import hireJavascriptDevelopers from './hire_javascript_developers'
-import hireNetDevelopers from './hire_net_developers'
-import websiteBuilders from './website_builders'
-import offshoreWebDevelopment from './offshore_web_development'
-import nearshoreNetDevelopers from './nearshore_net_developers'
-import nearshoreWebDevelopment from './nearshore_web_development'
-import webDevelopmentOutsourcing from './web_development_outsourcing'
-import nearshoreSoftwareOutsourcing from './nearshore_software_outsourcing'
-import devOps from './devops'
 import itOutsourcing from './it_outsourcing'
-import itStaffing from './it_staffing'
-import mobile from './mobile'
-import softwareDevelopment from './software_development'
 
 const variants = [
-  staffAugmentation,
-  webDevelopers,
-  webCoders,
-  webProgrammers,
-  hireDevelopers,
-  hireJavascriptDevelopers,
-  hireNetDevelopers,
-  websiteBuilders,
-  offshoreWebDevelopment,
-  nearshoreNetDevelopers,
-  nearshoreWebDevelopment,
-  webDevelopmentOutsourcing,
-  nearshoreSoftwareOutsourcing,
-  devOps,
-  itOutsourcing,
-  itStaffing,
-  mobile,
-  softwareDevelopment
+  itOutsourcing
 ]
 
 export default variants
