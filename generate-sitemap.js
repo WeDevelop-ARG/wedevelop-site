e0ffc52dfbb563be5c275fe7a3f6b894f633f154
Robertino Nicolazzi <robertino.nicolazzi@gmail.com>
2022-01-31 14:49:43 -0300

    generate sitemap

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,10 +1,11 @@
 const fs = require('fs')
 const globby = require('globby')
+require('dotenv').config()
 
 function addPage (page) {
   const path = page.replace('pages', '').replace('.jsx', '')
   const route = path === '/index' ? '' : path
-  return `  <url>
+  return `<url>
     <loc>${`${process.env.NEXT_PUBLIC_BASE_URL}${route}`}</loc>
     <lastmod>${new Date().toISOString()}</lastmod>
     <changefreq>monthly</changefreq>
@@ -19,12 +20,13 @@ async function generateSitemap () {
     '!pages/_*.jsx'
   ])
 
-  console.log(pages)
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${pages.map(addPage).join('\n')}
   </urlset>`
 
   fs.writeFileSync('public/sitemap.xml', sitemap)
+
+  console.log('Sitemap generated successfully')
 }
 generateSitemap()
