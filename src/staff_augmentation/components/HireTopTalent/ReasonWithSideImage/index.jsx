0a8de7385fe45111abd8264bbb00637523c94974
Hiram J. González <hiram.gonzalez@wedevelop.me>
2022-03-08 16:44:35 -0400

    Apply responsive design to new HireTopTalent section

--- src/staff_augmentation/components/HireTopTalent/ReasonWithSideImage/index.jsx
+++ src/staff_augmentation/components/HireTopTalent/ReasonWithSideImage/index.jsx
@@ -1,15 +1,20 @@
 import { useCallback, useState } from 'react'
 import classNames from 'classnames'
 
+import useMediaQuery from 'utils/use_media_query'
+
 import Button from 'main_app/components/Button'
 import Image from 'main_app/components/Image'
 
 import BlueArrow from 'assets/portfolio/arrow.component.svg'
 import BlueTopArrow from 'assets/reasons_to_choose_wedev/top_arrow.component.svg'
 
+import { forPhoneOnly } from 'styles/media_queries'
+
 import classes from './styles.module.scss'
 
 function ReasonWithSideImage ({ isRightAlignment, imageURL, title, description, toggleContent }) {
+  const isPhoneOnly = useMediaQuery(forPhoneOnly)
   const [displayToggleContainer, setDisplayToggleContainer] = useState(false)
   const [hideSeeMoreButton, setHideSeeMoreButton] = useState(false)
   const handleShowToggleContainer = useCallback(() => {
@@ -23,7 +28,7 @@ function ReasonWithSideImage ({ isRightAlignment, imageURL, title, description, 
 
   return (
     <section className={classes.container}>
-      <div className={isRightAlignment ? classes.imageContainerToRight : classes.imageContainerToLeft}>
+      <div className={isRightAlignment && !isPhoneOnly ? classes.imageContainerToRight : classes.imageContainerToLeft}>
         <Image src={imageURL} alt='' className={classes.reasonImage} />
       </div>
       <div className={classNames(classes.reasonContent, {
@@ -31,19 +36,19 @@ function ReasonWithSideImage ({ isRightAlignment, imageURL, title, description, 
       })}
       >
         <h2 className={classNames(classes.title, {
-          [classes.textAlignToRight]: isRightAlignment
+          [classes.textAlignToRight]: isRightAlignment && !isPhoneOnly
         })}
         >
           {title}
         </h2>
         <p className={classNames(classes.description, {
-          [classes.textAlignToRight]: isRightAlignment
+          [classes.textAlignToRight]: isRightAlignment && !isPhoneOnly
         })}
         >
           {description}
         </p>
         <div className={classNames(classes.toggleContainer, {
-          [classes.textAlignToRight]: isRightAlignment,
+          [classes.textAlignToRight]: isRightAlignment && !isPhoneOnly,
           [classes.displayToggleContainer]: displayToggleContainer
         })}
         >
@@ -53,7 +58,7 @@ function ReasonWithSideImage ({ isRightAlignment, imageURL, title, description, 
           variant='link'
           iconRight={<BlueArrow />}
           className={classNames(classes.seeButton, {
-            [classes.seeButtonToRight]: isRightAlignment,
+            [classes.seeButtonToRight]: isRightAlignment && !isPhoneOnly,
             [classes.hideSeeButton]: hideSeeMoreButton
           })}
           onClick={() => handleShowToggleContainer()}
@@ -64,7 +69,7 @@ function ReasonWithSideImage ({ isRightAlignment, imageURL, title, description, 
           variant='link'
           iconRight={<BlueTopArrow />}
           className={classNames(classes.seeButton, {
-            [classes.seeButtonToRight]: isRightAlignment,
+            [classes.seeButtonToRight]: isRightAlignment && !isPhoneOnly,
             [classes.hideSeeButton]: !hideSeeMoreButton
           })}
           onClick={() => handleHideToggleContainer()}
