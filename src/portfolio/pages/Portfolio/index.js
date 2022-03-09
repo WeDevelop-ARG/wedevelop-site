99d9e7dc0369abc357e6188adcf7bde62c29eba0
Jo Carrasco <jogregcarrasco@gmail.com>
2022-03-09 14:48:46 -0400

    feat(portfolio): Add section for display

--- src/portfolio/pages/Portfolio/index.js
+++ src/portfolio/pages/Portfolio/index.js
@@ -11,8 +11,9 @@ import NavBar from 'main_app/components/NavBar'
 import PictureWall from 'main_app/components/PictureWall'
 import PortfolioHeader from 'portfolio/components/PortfolioHeader'
 import PortfolioProjectCard from 'portfolio/components/PortfolioProjectCard'
-
+import PortfolioHeaderBackground from 'assets/portfolio/portfolio_header_background.svg'
 import PortfolioImageSeparator from 'assets/portfolio/portfolio_image_separator.png'
+import InternalLink from 'main_app/components/InternalLink'
 
 import classes from './styles.module.scss'
 import { useRouter } from 'next/router'
@@ -21,7 +22,6 @@ function Portfolio () {
   const { stories } = useStories()
   const contactPagePath = '/portfolio/contact'
   const { pathname, push } = useRouter()
-
   const handleClose = useCallback(() => {
     push('/career', undefined, { shallow: true, scroll: false })
   }, [push])
@@ -63,6 +63,41 @@ function Portfolio () {
           <div className={classes.bottomSmallBlurCircle} />
           {pathname === contactPagePath && <ContactModal isOpen onRequestClose={handleClose} />}
         </section>
+        <div className={classes.portfolioProjectPreShowcase} style={{ backgroundImage: `url(${PortfolioHeaderBackground.src})` }}>
+          <div className={classes.portfolioFeatured}>
+            <h3>Our Portfolio</h3>
+            <h4>See what's possible</h4>
+            <p>More than 20 companies have trust on us!</p>
+            <hr className={classes.horizontalBar} />
+            <InternalLink
+              href='/portfolio'
+              className={classes.container}
+            >
+              <button>View More</button>
+            </InternalLink>
+          </div>
+          <div className={classes.portfolioProjectDisplayWrapper}>
+            {stories.map((story, i) => {
+              if (i < 3) {
+                return (
+                  <InternalLink
+                    href={`/portfolio/${story.urlName}`}
+                    className={classes.container}
+                  >
+                    <div className={classes.portfolioProjectPreview} key={i} style={{ backgroundImage: `url(${story.header.imageURL.src})` }}>
+                      <div className={classes.portfolioProjectCompanyLogo} style={{ backgroundColor: story.header.logoBackground }}>
+                        <img src={story.header.logoURL.src} alt={`logo-${story.urlName}`} />
+                      </div>
+                    </div>
+                  </InternalLink>
+                )
+              } else {
+                return null
+              }
+            }
+            )}
+          </div>
+        </div>
         <BackgroundContainer backgroundURL={PortfolioImageSeparator} />
         <GetInTouch contactPagePath={contactPagePath} />
         <PictureWall />
