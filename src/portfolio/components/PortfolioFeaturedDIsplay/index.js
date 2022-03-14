e9c742519fea1cb0155a349c9d018d0b5b2e7732
JoCarrasco <jogregcarrasco@gmail.com>
2022-03-14 10:10:41 -0400

    fix(portfolio): Remove svg background and added css implementation of gradient

--- src/portfolio/components/PortfolioFeaturedDIsplay/index.js
+++ src/portfolio/components/PortfolioFeaturedDIsplay/index.js
@@ -1,4 +1,3 @@
-import PortfolioFeaturedProjectsBackground from 'assets/portfolio/portfolio_featured_projects_bg.svg'
 import Button from 'main_app/components/Button'
 import InternalLink from 'main_app/components/InternalLink'
 import WrappedImage from 'main_app/components/WrappedImage'
@@ -12,55 +11,56 @@ function PortfolioFeatureDisplay ({ title, subtitle, description, buttonText }) 
   return (
     <div
       className={classes.portfolioProjectPreShowcase}
-      style={{ backgroundImage: `url(${PortfolioFeaturedProjectsBackground.src})` }}
     >
-      <div className={classes.portfolioFeatured}>
-        <h3>{title}</h3>
-        <h4>{subtitle}</h4>
-        <p>{description}</p>
-        <hr className={classes.horizontalBar} />
-        <Button
-          as={InternalLink}
-          href='/portfolio'
-          isAnchor
-          variant={['secondary', 'light']}
-          className={classes.viewMoreButton}
-        >
-          {buttonText}
-        </Button>
-      </div>
-      <div className={classes.portfolioProjectDisplayWrapper}>
-        {stories.slice(0, 3).map((story, i) => {
-          return (
-            <InternalLink
-              key={i}
-              href={`/portfolio/${story.urlName}`}
-              className={classes.container}
-            >
-              <div className={classes.portfolioProjectWrapper}>
-                <WrappedImage
-                  layout='fill'
-                  src={story.header.imageURL.src}
-                  alt=''
-                  width={defaultProjectPreviewImgSizeInPx}
-                  height={defaultProjectPreviewImgSizeInPx}
-                  className={classes.portfolioProjectPreview}
-                />
-                <div className={classes.portfolioProjectCompanyLogoWrapper} style={{ backgroundColor: story.header.logoBackground }}>
+      <div className={classes.portfolioFeaturedWrapper}>
+        <div className={classes.portfolioFeatured}>
+          <h3>{title}</h3>
+          <h4>{subtitle}</h4>
+          <p>{description}</p>
+          <hr className={classes.horizontalBar} />
+        </div>
+        <div className={classes.portfolioProjectDisplayWrapper}>
+          {stories.slice(0, 3).map((story, i) => {
+            return (
+              <InternalLink
+                key={i}
+                href={`/portfolio/${story.urlName}`}
+                className={classes.container}
+              >
+                <div className={classes.portfolioProjectWrapper}>
                   <WrappedImage
                     layout='fill'
-                    src={story.header.logoURL.src}
-                    alt={`logo-${story.urlName}`}
-                    width={defaultCompanyLogoImgSizeInPx}
-                    height={defaultCompanyLogoImgSizeInPx}
-                    className={classes.portfolioProjectCompanyLogo}
+                    src={story.header.imageURL.src}
+                    alt=''
+                    width={defaultProjectPreviewImgSizeInPx}
+                    height={defaultProjectPreviewImgSizeInPx}
+                    className={classes.portfolioProjectPreview}
                   />
+                  <div className={classes.portfolioProjectCompanyLogoWrapper} style={{ backgroundColor: story.header.logoBackground }}>
+                    <WrappedImage
+                      layout='fill'
+                      src={story.header.logoURL.src}
+                      alt={`logo-${story.urlName}`}
+                      width={defaultCompanyLogoImgSizeInPx}
+                      height={defaultCompanyLogoImgSizeInPx}
+                      className={classes.portfolioProjectCompanyLogo}
+                    />
+                  </div>
                 </div>
-              </div>
-            </InternalLink>
-          )
-        })}
+              </InternalLink>
+            )
+          })}
+        </div>
       </div>
+      <Button
+        as={InternalLink}
+        href='/portfolio'
+        isAnchor
+        variant={['secondary', 'light']}
+        className={classes.viewMoreButton}
+      >
+        {buttonText}
+      </Button>
     </div>
   )
 }
