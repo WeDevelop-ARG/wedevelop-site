fa5d871236428ee160482f3504ffc20c5f2b1089
Hiram J. González <hiram.gonzalez@wedevelop.me>
2022-03-18 14:50:17 -0400

    Create Question component

---
+++ src/staff_augmentation/components/FAQs/components/Question/index.js
@@ -0,0 +1,48 @@
+import { useCallback, useState } from 'react'
+
+import Button from 'main_app/components/Button'
+
+import PlusIcon from 'assets/staff_augmentation/faqs/plus_icon.component.svg'
+import MinusIcon from 'assets/staff_augmentation/faqs/minus_icon.component.svg'
+
+import classes from './styles.module.scss'
+import classNames from 'classnames'
+
+export default function Question ({ question, answer }) {
+  const [displayAnswer, setDisplayAnswer] = useState(false)
+  const [displaySeeLessIcon, setDisplaySeeLessIcon] = useState(false)
+  const handleShowToggleContainer = useCallback(() => {
+    setDisplaySeeLessIcon(true)
+    setDisplayAnswer(true)
+  }, [setDisplayAnswer, setDisplaySeeLessIcon])
+  const handleHideToggleContainer = useCallback(() => {
+    setDisplaySeeLessIcon(false)
+    setDisplayAnswer(false)
+  }, [setDisplayAnswer, setDisplaySeeLessIcon])
+  console.log(question, answer)
+  return (
+    <div className={classes.container}>
+      <Button
+        variant='link'
+        iconRight={displaySeeLessIcon ? <MinusIcon /> : <PlusIcon />}
+        onClick={() => {
+          if (!displayAnswer) {
+            handleShowToggleContainer()
+          } else {
+            handleHideToggleContainer()
+          }
+        }}
+        className={classes.displayButton}
+      >
+        {question}
+      </Button>
+      <div className={classNames(classes.answer, {
+        [classes.displayAnswer]: displayAnswer
+      })}
+      >
+        {answer}
+      </div>
+      <hr className={classes.separator} />
+    </div>
+  )
+}
