5fbbce794ad85a9583bb365b3696927b5fd39ee3
JoCarrasco <jogregcarrasco@gmail.com>
2022-04-05 11:37:36 -0400

    feat(img): Add AMP support for images
    Note: WIP change, breaking change

--- src/main_app/components/Image/index.jsx
+++ src/main_app/components/Image/index.jsx
@@ -1,11 +1,18 @@
-import NextImage from 'next/image'
+import * as Amp from 'react-amphtml'
 
 export default function Image ({ src, ...props }) {
   const imageURL = typeof src === 'string' ? src : src?.src
+  if (src.height && props.height === undefined) { props.height = src.width }
+  if (src.width && props.width === undefined) { props.width = src.width }
+
+  if (props.layout === undefined) { props.layout = 'responsive' }
+  if (props.width === undefined) { props.width = 14 }
+  if (props.height === undefined) { props.height = 14 }
 
   return (
-    <NextImage
-      src={src}
+    <Amp.AmpImg
+      src={imageURL}
+      specName='default'
       unoptimized={imageURL?.endsWith('.svg')}
       {...props}
     />
