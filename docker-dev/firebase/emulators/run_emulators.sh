27c2f284e725a1a2d18d2ebfffc6bc73454c58ba
Sergio Shevtsov <sergioshev@gmail.com>
2022-04-07 01:18:48 +0300

    removed BUILD_PID from wait

--- docker-dev/firebase/emulators/run_emulators.sh
+++ docker-dev/firebase/emulators/run_emulators.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
 declare -ig SIGTERM_RECEIVED=0
-declare -ig BUILD_PID=0
+#declare -ig BUILD_PID=0
 declare -ig FIREBASE_PID=0
+
 _handle_SIGTERM () {
   if [ $SIGTERM_RECEIVED -ne 1 ]; then
-    echo "Received SIGTERM for the first time, stopping sub-processes: $BUILD_PID $FIREBASE_PID"
+    echo "Received SIGTERM for the first time, stopping sub-processes: $FIREBASE_PID"
     SIGTERM_RECEIVED=1
-    kill $BUILD_PID $FIREBASE_PID
+    kill $FIREBASE_PID
   else
     echo "Received another SIGTERM, ignoring..."
   fi
 }
 
 _handle_SIGKILL () {
-  echo "Received SIGKILL, sending another SIGTERM to sub-processes: $BUILD_PID $FIREBASE_PID"
-  kill $BUILD_PID $FIREBASE_PID
+  echo "Received SIGKILL, sending another SIGTERM to sub-processes: $FIREBASE_PID"
+  kill $FIREBASE_PID
 }
 
 trap -- '_handle_SIGTERM' TERM
 trap -- '_handle_SIGKILL' KILL
 
-
 runFirebaseEmulators () {
-  firebase emulators:start \
+  FIREBASE_AUTH_EMULATOR_HOST="localhost:9099" firebase emulators:start \
     --only functions,storage \
     --project demo-test \
     --export-on-exit \
@@ -33,11 +33,11 @@ runFirebaseEmulators () {
 
 runFirebaseEmulators
 
-echo "Waiting for sub-processes to finish: $BUILD_PID $FIREBASE_PID"
-wait $BUILD_PID $FIREBASE_PID
+echo "Waiting for sub-processes to finish:  $FIREBASE_PID"
+wait $FIREBASE_PID
 while [ $? -ge 128 ]; do
-  echo "Still waiting for sub-processes to finish: $BUILD_PID $FIREBASE_PID"
-  wait $BUILD_PID $FIREBASE_PID
+  echo "Still waiting for sub-processes to finish: $FIREBASE_PID"
+  wait $FIREBASE_PID
 done
 
 echo "All sub-processes finished"
