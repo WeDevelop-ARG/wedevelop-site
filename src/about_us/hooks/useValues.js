d0a5443cb4cba31b27484837ed06e450b8aa14eb
JoCarrasco <jogregcarrasco@gmail.com>
2022-04-06 09:43:52 -0400

    docs: change text in 'about-us'

--- src/about_us/hooks/useValues.js
+++ src/about_us/hooks/useValues.js
@@ -1,12 +1,12 @@
 const values = [{
   id: '1',
   title: 'People first',
-  description: <>We provide a workplace of <b>trust</b>, <b>respect</b>, and <b>fellowship</b></>
+  description: <>We provide a workplace of <b>trust</b>, <b>respect</b>, and <b>camaraderie</b></>
 },
 {
   id: '2',
   title: 'Relationship building',
-  description: <>We work every day on creating <b>solid relationships</b></>
+  description: <>We work every day to create <b>dependable relationships</b></>
 },
 {
   id: '3',
@@ -16,17 +16,17 @@ const values = [{
 {
   id: '4',
   title: 'Progress visibility',
-  description: <><b>Openness and transparency</b> to develop your project</>
+  description: <>We offer <b>total transparency </b>for every step of your project</>
 },
 {
   id: '5',
   title: 'Crystal-clear communication',
-  description: <>We believe that <b>proactive communication</b> is the key for teamwork</>
+  description: <>We believe that <b>proactive communication</b> is the key to teamwork</>
 },
 {
   id: '6',
   title: 'Openness',
-  description: <><b>All obstacles are discussed openly.</b>They are improvement opportunities</>
+  description: <><b>We discuss all issues openly. </b>They are valuable opportunities for improvement</>
 }
 ]
 
