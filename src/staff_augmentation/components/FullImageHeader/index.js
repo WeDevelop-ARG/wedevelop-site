864e72513883d26d70fec4b8df85625243a71fa1
JoCarrasco <jogregcarrasco@gmail.com>
2022-04-11 13:39:07 -0400

    feat: add submit functionality - add missing styles

--- src/staff_augmentation/components/FullImageHeader/index.js
+++ src/staff_augmentation/components/FullImageHeader/index.js
@@ -1,3 +1,5 @@
+import { useCallback } from 'react'
+import { useRouter } from 'next/router'
 
 import AbelBookBackground from 'assets/staff_augmentation/abel_book_background.png'
 import DotsPattern from 'assets/staff_augmentation/white_dots_pattern.svg'
@@ -12,17 +14,16 @@ import classes from './styles.module.scss'
 import ScheduleFormBasic from '../ScheduleFormBasic'
 
 export default function FullImageHeader ({
-  // freeQuoteForm,
-  // landingName,
-  // fixedForm,
-  // backgroundColor,
   title,
   description,
   onScheduleMeetingCTAClick
-  // onContactCTAClick,
-  // quote = ''
 }) {
-  // const [isClutchLoaded, setIsClutchLoaded] = useState(false)
+  const { push } = useRouter()
+  const onSuccess = useCallback(() => {
+    const redirectUrl = '/success/confirm'
+    push(redirectUrl)
+  }, [push])
+
   return (
     <>
       <section id='headerSection' className={classes.headerContainer}>
@@ -48,7 +49,7 @@ export default function FullImageHeader ({
             <div className={classes.hideOnTabletDown}>
               <h2 className={classes.subTitle}>{description}</h2>
               <div className={classes.formWrapper}>
-                <ScheduleFormBasic />
+                <ScheduleFormBasic onSubmit={onSuccess} />
                 <p className={classes.alternativeSchedule}>
                   Or, use{' '}
                   <Button variant={['link', 'focus-scale']} className={classes.scheduleFormButton} onClick={onScheduleMeetingCTAClick}>
@@ -57,63 +58,11 @@ export default function FullImageHeader ({
                   {' '}to schedule a call with us.
                 </p>
               </div>
-              {/* <ClutchRectangle
-                className={classes.desktopClutch}
-                isClutchLoaded={isClutchLoaded}
-                setIsClutchLoaded={setIsClutchLoaded}
-              /> */}
             </div>
           </div>
           <div className={classes.rightSideContainer}>
             <WrappedImage src={DotsPattern} alt='' loading='eager' layout='intrinsic' className={classes.dotsPatternRight} />
             <p className={classes.imageSubject}>Abel Osorio<br /> Chief Executive Officer</p>
-
-            {/* <div className={classes.ctaContainer}>
-              <div className={classes.ctaButtonContainer}>
-                <Button className={classes} onClick={onContactCTAClick} variant={['primary', 'focus-gradient']}>
-                  Get in touch
-                </Button>
-              </div>
-              <p className={classes.alternativeSchedule}>
-                Or, use{' '}
-                <Button variant={['link', 'focus-scale']} className={classes.scheduleFormButton} onClick={onScheduleMeetingCTAClick}>
-                  this link
-                </Button>
-                {' '}to schedule a call with us.
-              </p>
-              <div className={classes.whatHappensNextContainer}>
-                <OverlayTrigger
-                  trigger={['hover', 'focus']}
-                  placement='left'
-                  overlay={
-                    <Popover id='what-happens-next-popover' style={{ zIndex: 10 }}>
-                      <div className={classes.popoverContainer}>
-                        <Popover.Title>
-                          WHAT HAPPENS NEXT?
-                        </Popover.Title>
-                        <Popover.Content>
-                          Our solution experts will answer your questions in a secure online meeting. You will get good information and honest advice in plain English. You are then free to choose how to move forward
-                        </Popover.Content>
-                      </div>
-                    </Popover>
-                  }
-                >
-                  <div tabIndex='0' className={classes.whatHappensNext}>
-                    <WrappedImage className={classes.icon} src={InfoIcon} alt='' /> WHAT HAPPENS NEXT?
-                  </div>
-                </OverlayTrigger>
-              </div>
-            </div>
-            <div className={classes.hideOnDesktopUp}>
-              <div className={classes.separator} />
-              <ClutchRectangle
-                isClutchLoaded={isClutchLoaded}
-                setIsClutchLoaded={setIsClutchLoaded}
-              />
-              <InternalLink href='#latam-talent-section' className={classes.arrowDown}>
-                <Image src={ArrowDown} layout='fixed' alt='' />
-              </InternalLink>
-            </div> */}
           </div>
         </div>
       </section>
