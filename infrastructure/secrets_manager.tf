f9bb2582114a210d1a816a67ab1c5017c208565b
Sergio Shevtsov <sergioshev@gmail.com>
2022-04-20 17:13:47 +0300

    TF pr suggestion

--- infrastructure/secrets_manager.tf
+++ infrastructure/secrets_manager.tf
@@ -8,3 +8,15 @@ resource "google_secret_manager_secret" "cloud_functions_secrets" {
     automatic = true
   }
 }
+
+data "google_secret_manager_secret" "cloud_functions_secrets" {
+  depends_on = [
+    google_project_service.service["secretmanager.googleapis.com"]
+  ]
+  secret_id = "${local.project_id}-cloud-functions"
+}
+
+data "google_secret_manager_secret_version" "cloud_functions_secrets" {
+  secret = data.google_secret_manager_secret.cloud_functions_secrets.secret_id
+  version = "latest"
+}
