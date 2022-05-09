e4c513525bd5b11abd40de328e5c1c33caaf100b
Nahuel Dallacamina <nahudalla@gmail.com>
2022-05-09 11:23:04 -0300

    Remove landing sections

--- src/staff_augmentation/landing_page_content.js
+++ src/staff_augmentation/landing_page_content.js
@@ -3,10 +3,7 @@ import Footer from './components/Footer'
 import FullSizeCTA from './components/FullSizeCTA'
 import HowDoesItWorks from 'main_app/components/HowDoesItWorks'
 import PictureWall from 'main_app/components/PictureWall'
-import ReviewCards from './components/ReviewCards'
-import SkillSet from './components/SkillSet'
 
-import PortfolioFeatureDisplay from 'portfolio/components/PortfolioFeaturedDIsplay'
 import FrequentlyAskedQuestions from './components/FAQs'
 import OurServices from './components/OurServices'
 import FiguresFacts from './components/FiguresFacts'
@@ -24,27 +21,6 @@ export default function LandingPageContent ({
         <HowDoesItWorks
           handleContactCTAClick={handleContactCTAClick}
           handleScheduleMeetingCTAClick={handleScheduleMeetingCTAClick}
-        />
-        <ReviewCards
-          subtitle={landing.reviewsHeading.subtitle}
-          title={landing.reviewsHeading.title}
-          description={landing.reviewsHeading.description}
-          reviews={landing.reviews}
-          buttonText={landing.reviewsHeading.buttonText}
-          handleModal={handleContactCTAClick}
-        />
-        <SkillSet
-          title={landing.skillSet?.title}
-          subtitle={landing.skillSet?.subtitle}
-          ctaText={landing.skillSet?.ctaText}
-          skillSet={landing.skillSet?.positions}
-          onCTAClick={handleContactCTAClick}
-        />
-        <PortfolioFeatureDisplay
-          title={landing.portfolioFeaturedDisplay.title}
-          subtitle={landing.portfolioFeaturedDisplay.subtitle}
-          description={landing.portfolioFeaturedDisplay.description}
-          buttonText={landing.portfolioFeaturedDisplay.buttonText}
         />
         <FrequentlyAskedQuestions
           icon={landing.FAQs.icon}
