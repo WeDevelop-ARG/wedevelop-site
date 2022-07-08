3ecf74cd9e5b76d4b28f0245ac713c1f432c08ae
Ricardo Pérez <ricardopp1992@gmail.com>
2022-07-07 21:11:33 -0400

    added hubspot form on careers page, submit finished is not ready

--- src/careers/components/JoinUsForm/index.js
+++ src/careers/components/JoinUsForm/index.js
@@ -1,175 +1,58 @@
-import { useState, useCallback } from 'react'
-import isUndefined from 'lodash/isUndefined'
-import classnames from 'classnames'
-import { Field, ErrorMessage } from 'formik'
-import InputGroup from 'react-bootstrap/InputGroup'
+import { useCallback, useState } from 'react'
+import { useRouter } from 'next/router'
 
-import SubmitButton from 'main_app/components/SubmitButton'
-import Textarea from 'main_app/components/Textarea'
-import FormikSelect from 'main_app/components/FormikSelect'
-import ReCAPTCHAField from 'main_app/components/ReCAPTCHAField'
+import {
+  LANDING_CAREERS_HUBSPOT_FORM_FORM_ID,
+  LANDING_FREE_QUOTE_HUBSPOT_FORM_PORTAL_ID,
+  LANDING_FREE_QUOTE_HUBSPOT_FORM_REGION,
+} from 'main_app/constants'
 import WrappedImage from 'main_app/components/WrappedImage'
+import LoaderSpinner from 'main_app/components/LoaderSpinner'
 
-import FileInput from './FileInput'
-import UploadingFile from 'staff_augmentation/components/UploadingFile'
-import ErrorUploadingFile from 'staff_augmentation/components/ErrorUploadingFile'
-
-import useSkillOptions from 'main_app/components/useSkillOptions'
-import useFieldWithErrorClassName from 'utils/use_field_with_error_class_name'
-
-import FormLogic from './FormLogic'
-import CountrySelect from './CountrySelect'
+import HubspotFreeQuoteForm from 'staff_augmentation/components/HubspotFreeQuoteForm'
 
 import DotsPattern from 'assets/about_us/dots_pattern.svg'
-import Clip from 'assets/career/clip.svg'
 import HalfCircle from 'assets/home/services/half_circle.svg'
 
 import classes from './styles.module.scss'
 
-function JoinUsForm () {
-  const skillOptions = useSkillOptions()
-  const TextAreaWithError = useFieldWithErrorClassName(
-    Textarea,
-    classes.fieldWithError
-  )
-  const InputWithError = useFieldWithErrorClassName(
-    'input',
-    classes.fieldWithError
-  )
+function JoinUsForm() {
+  const [isLoading, setIsLoading] = useState(true)
+  const { push } = useRouter()
 
-  const [isSubmitted, setIsSubmitted] = useState(false)
-  const [error, setError] = useState()
-  const [fileUploadProgress, setFileUploadProgress] = useState()
-  const [fileUploadError, setFileUploadError] = useState()
+  const handleSubmitFinished = useCallback(async values => {
+    console.log(values);
 
-  const onSubmitFinished = useCallback((err) => {
-    setIsSubmitted(true)
-    setError(err)
+    // onSuccess()
   }, [])
 
+  const onLoadingStateChange = useCallback(isLoading => {
+    setIsLoading(isLoading)
+  }, [])
+
+  const onSuccess = useCallback(() => {
+    const redirectUrl = '/success/confirm'
+    push(redirectUrl)
+  }, [push])
+
   return (
-    <section className={classes.joinUsSection}>
+    <section className={classes.joinUsSection} >
       <p className={classes.subheadingText}>Join Us</p>
       <h2 className={classes.titleText}>Let’s grow together</h2>
       <hr className={classes.horizontalBar} />
-      <FormLogic
-        onSubmitFinished={onSubmitFinished}
-        className={classes.form}
-        setFileUploadProgress={setFileUploadProgress}
-        setFileUploadError={setFileUploadError}
-      >
-        <label className={classes.labels}>
-          <span>*</span>{' '}First Name
-          <Field
-            as={InputWithError}
-            type='text'
-            name='firstName'
-            className={classes.inputStyles}
-          />
-        </label>
-        <label className={classes.labels}>
-          <span>*</span>{' '}Last Name
-          <Field
-            as={InputWithError}
-            type='text'
-            name='lastName'
-            className={classes.inputStyles}
-          />
-        </label>
-        <label className={classes.labels}>
-          <span>*</span>{' '}Email
-          <Field
-            as={InputWithError}
-            type='email'
-            name='email'
-            className={classes.inputStyles}
-          />
-        </label>
-        <label className={classes.labels}>
-          <span>*</span>{' '}Skills
-          <FormikSelect
-            name='skills'
-            options={skillOptions}
-            placeholder='Select your skills'
-          />
-          <ErrorMessage name='skills' component='div' className={classes.errorMessage} />
-        </label>
-        <label className={classes.labels}>
-          <span>*</span>{' '}Where are you from?
-          <CountrySelect
-            name='country'
-            placeholder='Select your country'
-          />
-          <ErrorMessage name='country' component='div' className={classes.errorMessage} />
-        </label>
-        <label className={classes.labels}>
-          Intended Remuneration (monthly)
-          <InputGroup className={classes.remunerationGroup}>
-            <InputGroup.Text className={classes.currency}>$</InputGroup.Text>
-            <Field
-              as={InputWithError}
-              type='text'
-              name='remuneration'
-              placeholder='0.0'
-              className={classes.remuneration}
-            />
-          </InputGroup>
-        </label>
-        <label className={classes.labels}>
-          In one sentence, tell us what makes you unique
-          <Field
-            as={TextAreaWithError}
-            name='message'
-            maxLength='200'
-            className={classnames(classes.inputStyles, classes.textarea)}
-          />
-        </label>
-        <label className={classes.fileUploadField}>
-          <span className={classes.fileUploadLabel}>Resume/CV</span>
-          {(isUndefined(fileUploadProgress) && isUndefined(fileUploadError)) &&
-            <Field
-              name='resume'
-              component={FileInput}
-              aria-hidden='true'
-            >
-              <div className={classes.field}>
-                <WrappedImage src={Clip} layout='fixed' className={classes.clip} alt='' />
-                Attach
-              </div>
-            </Field>}
-          {(!isUndefined(fileUploadProgress) && isUndefined(fileUploadError)) &&
-            <UploadingFile
-              inputName='resume'
-              currentProgress={fileUploadProgress}
-            />}
-          {!isUndefined(fileUploadError) &&
-            <ErrorUploadingFile
-              retryButtonProps={{
-                type: 'submit'
-              }}
-            />}
-          <ErrorMessage name='resume' component='div' className={classes.errorMessage} />
-        </label>
-        <ReCAPTCHAField name='recaptchaToken' />
-        {isSubmitted && error && (
-          <p className={classes.submitError}>
-            Oops! An error occurred and we couldn't receive your application. Please try again later.
-          </p>
-        )}
-        {isSubmitted && !error && (
-          <p className={classes.submitSuccessful}>
-            Your application has been received successfully. We'll contact you soon.
-          </p>
-        )}
-        <div className={classes.buttonContainer}>
-          <SubmitButton
-            variant='primary'
-            className={classes.buttonStyles}
-          >
-            Join Us
-          </SubmitButton>
-        </div>
-      </FormLogic>
+      <div className={classes.careerForm}>
+        <LoaderSpinner
+          className={classes.loadingContainer}
+          visible={isLoading}
+        />
+        <HubspotFreeQuoteForm
+          region={LANDING_FREE_QUOTE_HUBSPOT_FORM_REGION}
+          portalId={LANDING_FREE_QUOTE_HUBSPOT_FORM_PORTAL_ID}
+          formId={LANDING_CAREERS_HUBSPOT_FORM_FORM_ID}
+          onLoadingStateChange={onLoadingStateChange}
+          onSubmit={handleSubmitFinished}
+        />
+      </div>
       <div className={classes.filledCircle} aria-hidden='true' />
       <div className={classes.emptyCircle} aria-hidden='true' />
       <div className={classes.smallBlurCircle} aria-hidden='true' />
@@ -179,7 +62,7 @@ function JoinUsForm () {
       <div className={classes.leftFilledCircle} />
       <div className={classes.leftEmptyCircle} />
       <div className={classes.leftSmallCircle} />
-    </section>
+    </section >
   )
 }
 
