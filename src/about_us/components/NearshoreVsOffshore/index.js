3f06d9fedb27ba9aceabd115f05bc5e7b81021c0
Hiram J. González <hiram.gonzalez@wedevelop.me>
2022-11-23 17:35:15 -0400

    Add deco element to Nearshore Vs Offshore section

--- src/about_us/components/NearshoreVsOffshore/index.js
+++ src/about_us/components/NearshoreVsOffshore/index.js
@@ -2,6 +2,8 @@ import useNearshoreVsOffshoreTableInfo from '../../hooks/useNearshoreVsOffshoreT
 
 import NearshoreVsOffshoreTable from './NearshoreVsOffshoreTable'
 
+import DecoCircles from 'assets/about_us/nearshore_outsourcing/complete_deco_circle.component.svg'
+
 import classes from './styles.module.scss'
 
 export default function NearshoreVsOffshore () {
@@ -9,6 +11,12 @@ export default function NearshoreVsOffshore () {
 
   return (
     <section className={classes.nearshoreVsOffshoreSection}>
+      <DecoCircles
+        alt=''
+        viewBox='0 0 319 280'
+        preserveAspectRatio='xMidYMid meet'
+        className={classes.decoCircles}
+      />
       <div className={classes.header}>
         <h2 className={classes.title}>Nearshore Vs Offshore</h2>
         <p className={classes.description}>
