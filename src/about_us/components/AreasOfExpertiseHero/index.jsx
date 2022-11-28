11954db5aae08984536528e0a3c5f8b1c6ec54d5
adrian <adrian.scicchitano@wedevelop.me>
2022-11-28 14:40:49 -0300

    add areas of expertise page, hero, testimonials & get in touch

---
+++ src/about_us/components/AreasOfExpertiseHero/index.jsx
@@ -0,0 +1,38 @@
+import Button from 'main_app/components/Button'
+import InternalLink from 'main_app/components/InternalLink'
+import WrappedImage from 'main_app/components/WrappedImage'
+
+import dottedSemiCircle from 'assets/about_us/areas_of_expertise/dotted-semi-circle.svg'
+import heroBackground from 'assets/about_us/areas_of_expertise/hero-background.png'
+import DotsPattern from 'assets/about_us/dots_pattern.svg'
+import decoElements from 'assets/about_us/nearshore_outsourcing/deco-elements.svg'
+
+import classes from './styles.module.scss'
+
+export default function AreasOfExpertiseHero () {
+  return (
+    <>
+      <section className={classes.areasOfExpertise}>
+        <p className={classes.subheadingText}>Our Company</p>
+        <div className={classes.sectionHeader}>
+          <h2 className={classes.titleText}>Our Areas Of Expertise</h2>
+          <p className={classes.descriptionText}>
+            We help companies innovate and scale by embracing the latest technologies and resources
+          </p>
+        </div>
+        <hr className={classes.horizontalBar} />
+        <Button
+          as={InternalLink}
+          href='/about-us/areas-of-expertise/contact'
+          className={classes.letsTalkButton}
+        >
+          Let's Talk!
+        </Button>
+        <WrappedImage src={DotsPattern} alt='' layout='intrinsic' className={classes.dotsPattern} aria-hidden='true' />
+        <WrappedImage src={decoElements} alt='' layout='intrinsic' className={classes.decoElements} aria-hidden='true' />
+        <WrappedImage src={dottedSemiCircle} alt='' layout='intrinsic' className={classes.leftDottedSemiCircle} aria-hidden='true' />
+      </section>
+      <WrappedImage src={heroBackground} alt='' layout='responsive' className={classes.backgroundImage} />
+    </>
+  )
+}
