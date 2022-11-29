9e5465322f9537c32df9ccdd45e822c2ec8e5cc7
Hiram J. González <hiram.gonzalez@wedevelop.me>
2022-11-29 17:34:22 -0400

    Change navbar name for Who We Are section

--- src/main_app/components/NavBar/MainMenu/index.js
+++ src/main_app/components/NavBar/MainMenu/index.js
@@ -121,14 +121,14 @@ function MainMenu ({
               href='/about-us'
               className={classes.dropdownItem}
             >
-              About Us
+              Who we are
             </Dropdown.Item>
             <Dropdown.Item
               as={InternalLink}
               href='/about-us/nearshore-outsourcing'
               className={classes.dropdownItem}
             >
-              Nearshore Outsourcing
+              Nearshore outsourcing
             </Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
