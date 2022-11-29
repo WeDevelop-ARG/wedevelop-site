c9a3c1372ad4df6845e70c2884fcc0200652b7e7
Hiram J. González <hiram.gonzalez@wedevelop.me>
2022-11-29 15:44:01 -0400

    Fix redirect when closing Contact modal in Our Technologies page

--- src/services/pages/OurTechnologies.js
+++ src/services/pages/OurTechnologies.js
@@ -2,15 +2,14 @@ import { useRouter } from 'next/router'
 import { useCallback } from 'react'
 
 import Article from 'main_app/components/Article'
-import PictureWall from 'main_app/components/PictureWall'
-import OurMainTechnologies from 'services/components/OurMainTechnologies'
-
-import Footer from 'main_app/components/Footer'
-import NavBar from 'main_app/components/NavBar'
-import PageMetadata from 'utils/marketing/PageMetadata'
-import OtherTechnologies from '../components/OtherTechnologies'
-import Schedule from '../components/schedule'
 import ContactModal from '../../main_app/components/ContactModal'
+import Footer from 'main_app/components/Footer'
+import PictureWall from 'main_app/components/PictureWall'
+import NavBar from 'main_app/components/NavBar'
+import OtherTechnologies from '../components/OtherTechnologies'
+import OurMainTechnologies from 'services/components/OurMainTechnologies'
+import PageMetadata from 'utils/marketing/PageMetadata'
+import Schedule from '../components/schedule'
 
 import classes from './services.module.scss'
 
@@ -19,7 +18,7 @@ function OurTechnologies () {
   const { pathname, push } = useRouter()
 
   const handleClose = useCallback(async () => {
-    await push('/services/web-development')
+    await push('/services/our-technologies')
   }, [push])
 
   return (
