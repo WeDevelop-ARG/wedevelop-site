4f73745068a612d94fb635c934c1a8de7c03e8c1
Hiram J. González <hiram.gonzalez@wedevelop.me>
2022-11-29 09:28:28 -0400

    Fix dot pattern overflow in HowStaffAugmentationSolutionsWork section

--- src/services/components/HowStaffAugmentationSolutionsWork/index.jsx
+++ src/services/components/HowStaffAugmentationSolutionsWork/index.jsx
@@ -4,27 +4,25 @@ import classes from './styles.module.scss'
 
 function HowStaffAugmentationSolutionsWork () {
   return (
-    <>
-      <section className={classes.howStaffAugmentationSolutionsWork}>
-        <DotsPattern className={classes.topRightPattern} viewBox='0 0 166 327' preserveAspectRatio='xMidYMid meet' />
-        <div className={classes.sectionContent}>
-          <h2 className={classes.titleContainer}>
-            <p className={classes.titleText}>How Our Staff Augmentation Solutions<b className={classes.colouredTitleText}> Work</b></p>
-          </h2>
-          <hr className={classes.horizontalBar} />
-          <p className={classes.descriptionText}>
-            Keep your business agile when developing your next project with our dependable IT staffing solutions. At WeDevelop,
-            <b> we help your business deliver value with a team aligned with your brand's goals and company culture.</b>
-            <br />
-            <br />
-            We understand the challenges of getting suitable hires on board, and that's why we streamline the hiring process,
-            <b> helping you save time and money</b>
-            . Our exclusive methodology focuses on tailoring our solutions and creating long-lasting relationships with every client.
-          </p>
-        </div>
-        <DotsPattern className={classes.leftPattern} viewBox='0 0 166 327' preserveAspectRatio='xMidYMid meet' />
-      </section>
-    </>
+    <section className={classes.howStaffAugmentationSolutionsWork}>
+      <DotsPattern className={classes.topRightPattern} viewBox='0 0 166 327' preserveAspectRatio='xMidYMid meet' />
+      <div className={classes.sectionContent}>
+        <h2 className={classes.titleContainer}>
+          <p className={classes.titleText}>How Our Staff Augmentation Solutions<b className={classes.colouredTitleText}> Work</b></p>
+        </h2>
+        <hr className={classes.horizontalBar} />
+        <p className={classes.descriptionText}>
+          Keep your business agile when developing your next project with our dependable IT staffing solutions. At WeDevelop,
+          <b> we help your business deliver value with a team aligned with your brand's goals and company culture.</b>
+          <br />
+          <br />
+          We understand the challenges of getting suitable hires on board, and that's why we streamline the hiring process,
+          <b> helping you save time and money</b>
+          . Our exclusive methodology focuses on tailoring our solutions and creating long-lasting relationships with every client.
+        </p>
+      </div>
+      <DotsPattern className={classes.leftPattern} viewBox='0 0 166 327' preserveAspectRatio='xMidYMid meet' />
+    </section>
   )
 }
 
