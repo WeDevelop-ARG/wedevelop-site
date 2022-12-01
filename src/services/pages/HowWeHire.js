b0dff0c1588dd778146e4b3abf9e366e12e127ea
Hiram J. González <hiram.gonzalez@wedevelop.me>
2022-12-01 17:54:42 -0400

    Implement Top3PercentageProfessionals section in How We Hire page

--- src/services/pages/HowWeHire.js
+++ src/services/pages/HowWeHire.js
@@ -1,23 +1,24 @@
-import Article from 'main_app/components/Article'
 import { useCallback } from 'react'
+import { useRouter } from 'next/router'
 
+import Article from 'main_app/components/Article'
 import ContactModal from 'main_app/components/ContactModal'
-import PictureWall from 'main_app/components/PictureWall'
-
 import Footer from 'main_app/components/Footer'
 import NavBar from 'main_app/components/NavBar'
 import PageMetadata from 'utils/marketing/PageMetadata'
-
-import { useRouter } from 'next/router'
+import PictureWall from 'main_app/components/PictureWall'
 import Schedule from 'services/components/schedule'
+import Top3PercentageProfessionals from '../components/Top3PercentageProfessionals'
+
+import classes from './services.module.scss'
 
 export default function HowWeHire () {
   const SERVICE_NAME = 'how-we-hire'
   const contactPagePath = '/services/how-we-hire/contact'
   const { pathname, push } = useRouter()
 
-  const handleClose = useCallback(() => {
-    push('/services/how-we-hire')
+  const handleClose = useCallback(async () => {
+    await push('/services/how-we-hire', undefined, { shallow: true, scroll: false })
   }, [push])
 
   return (
@@ -31,12 +32,16 @@ export default function HowWeHire () {
         contactPagePath={contactPagePath}
       />
       <Article>
-        {pathname === contactPagePath && <ContactModal isOpen onRequestClose={handleClose} />}
+        <Top3PercentageProfessionals
+          ctaLink={contactPagePath}
+        />
         <Schedule
           contactPagePath={contactPagePath}
           service={SERVICE_NAME}
+          className={classes.sectionContainer}
         />
         <PictureWall />
+        {pathname === contactPagePath && <ContactModal isOpen onRequestClose={handleClose} />}
       </Article>
       <Footer />
     </>
