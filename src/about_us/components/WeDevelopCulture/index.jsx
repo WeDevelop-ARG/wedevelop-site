e63a003ddb180ff196ad911cd0788299d9837750
Hiram J. González <hiram.gonzalez@wedevelop.me>
2022-12-12 16:34:32 -0400

    Improve WeDevelopCulture section in WhoWeAre page

--- src/about_us/components/WeDevelopCulture/index.jsx
+++ src/about_us/components/WeDevelopCulture/index.jsx
@@ -1,5 +1,6 @@
-import Image from 'main_app/components/Image'
+import WrappedImage from 'main_app/components/WrappedImage'
 
+import GradientBackground from 'assets/about_us/gradient-background.component.svg'
 import highValues from 'assets/about_us/high_values.svg'
 
 import classes from './styles.module.scss'
@@ -7,26 +8,24 @@ import classes from './styles.module.scss'
 export default function WeDevelopCulture () {
   return (
     <section className={classes.section}>
-      <div className={classes.content}>
-        <Image src={highValues} alt='high values decorative image' width='400px' />
-        <div className={classes.textContent}>
-          <h2 className={classes.title}>The WeDevelop Culture</h2>
-          <p className={classes.body}>
-            Working at WeDevelop is a fulfilling experience. We foster
-            collaboration among peers and respect everyone’s opinion,
-            creating an inviting and creative work environment.
-            <span className={classes.boldText}>
-              &nbsp;As team members feel appreciated and valued, WeDevelop is
-              the company everyone wants to work with. &nbsp;
-            </span>
-            We are always one step ahead of the game, ready to drive
-            change in the right direction and lead companies to
-            their next big step in their digital journeys.
-          </p>
-          <hr className={classes.horizontalBar} />
-        </div>
+      <div className={classes.textContent}>
+        <h2 className={classes.title}>The WeDevelop Culture</h2>
+        <hr className={classes.horizontalBar} />
+        <p className={classes.body}>
+          Working at WeDevelop is a fulfilling experience. We foster
+          collaboration among peers and respect everyone’s opinion,
+          creating an inviting and creative work environment.
+          <span className={classes.boldText}>
+            &nbsp;As team members feel appreciated and valued, WeDevelop is
+            the company everyone wants to work with. &nbsp;
+          </span>
+          We are always one step ahead of the game, ready to drive
+          change in the right direction and lead companies to
+          their next big step in their digital journeys.
+        </p>
       </div>
-      <div className={classes.background} />
+      <WrappedImage src={highValues} alt='high values decorative image' layout='intrinsic' />
+      <GradientBackground className={classes.background} viewBox='0 0 1680 729' preserveAspectRatio='xMidYMin slice' />
     </section>
   )
 }
