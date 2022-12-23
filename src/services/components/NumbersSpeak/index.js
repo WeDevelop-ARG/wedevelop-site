da0190734fa2ed789dda5b6b50028dfb38f045ed
adrian <adrian.scicchitano@wedevelop.me>
2022-12-23 18:13:03 -0300

    add tabs in staff agumentation page & wording fixes

--- src/services/components/NumbersSpeak/index.js
+++ src/services/components/NumbersSpeak/index.js
@@ -1,47 +1,103 @@
+import TabContainer from 'main_app/components/TabContainer'
 import BackgroundContainer from 'staff_augmentation/components/BackgroundContainer'
-import InternalLink from 'main_app/components/InternalLink'
-import WrappedImage from 'main_app/components/WrappedImage'
+import Image from 'main_app/components/Image'
 
-import DecoHalfCircle from 'assets/services/every-steps-half-circle-right.component.svg'
-import ListBullet from 'assets/services/benefits/bullet.svg'
+import TabButtonImage from 'assets/about_us/nearshore_outsourcing/tab_button.svg'
 import NumbersSpeakBackground from 'assets/services/background-numbers-speak.png'
+import DecoHalfCircle from 'assets/services/every-steps-half-circle-right.component.svg'
 
 import classes from './styles.module.scss'
 
 export default function NumbersSpeak () {
+  const renderTabLabel = (label) => (
+    <div className={classes.tabLabelContent}>
+      <Image src={TabButtonImage} alt='' width='20px' height='20px' />
+      <span className={classes.labelText}>{label}</span>
+    </div>
+  )
+
+  const tabs = [
+    {
+      label: renderTabLabel('75% of our clients hire the first match'),
+      children: (
+        <>
+          <p className={classes.tabText}>
+            And this is possible only because we love what we
+            do and strive for excellence in everything we do.
+          </p>
+          <br />
+          <p className={classes.tabText}>
+            Our staff works as a dream team where everyone is
+            a crucial player in the hiring journey.
+          </p>
+        </>
+      )
+    },
+    {
+      label: renderTabLabel('Top 3% Latin American Talent'),
+      children: (
+        <>
+          <p className={classes.tabText}>
+            We have skilled developers are from top 5 locations
+            are: Argentina, Brazil, Colombia, Uruguay, and Costa
+            Rica with matching timezone between
+            <span className={classes.boldText}>
+            &nbsp;GMT-6 to GMT-3
+            </span>
+            . Experts in top technologies we work with:
+            <ul className={classes.list}>
+              <li>
+                <b>Backend: </b>
+                <span>Node.js, Ruby on Rails, Python, Java, Golang.</span>
+              </li>
+              <li>
+                <b>Frontend: </b>
+                <span>React, Vue.js, Angular.</span>
+              </li>
+              <li>
+                <b>Mobile: </b>
+                <span>React Native, iOS.</span>
+              </li>
+              <li>
+                <b>QA Automation: </b>
+                <span>Selenium, Jenkins, Jest.</span>
+              </li>
+            </ul>
+          </p>
+        </>
+
+      )
+    },
+    {
+      label: renderTabLabel('15-day Money-back Trial Period'),
+      children: (
+        <>
+          <p className={classes.tabText}>
+            The Client may terminate our Agreement with
+            immediate effect by written notice to the other
+            Party during this trial period, and the services
+            rendered during such period shall not be invoiced.
+            If this provision is not exercised all services
+            shall be invoiced.
+          </p>
+        </>
+      )
+    }
+  ]
+
   return (
     <>
       <section className={classes.numbersSpeak}>
         <DecoHalfCircle className={classes.decoHalfCircle} viewBox='0 0 211 270' preserveAspectRatio='xMidYMid meet' />
-        <div className={classes.cardsAndButtons}>
-          <div className={classes.card}>
-            <WrappedImage alt='List bullet' src={ListBullet} className={classes.bullet} />
-            <p>Numbers speak for themselves</p>
-          </div>
-          <InternalLink
-            href='/services/staff-augmentation/#how-it-works'
-            className={classes.buttonCard}
-          >
-            <WrappedImage alt='List bullet' src={ListBullet} className={classes.bullet} />
-            <p>Building a remote software development team has never been easier</p>
-          </InternalLink>
-          <InternalLink
-            href='/services/staff-augmentation/#contact-us'
-            className={classes.buttonCard}
-          >
-            <WrappedImage alt='List bullet' src={ListBullet} className={classes.bullet} />
-            <p>Onboarding A Top-Notch Team</p>
-          </InternalLink>
-        </div>
-        <div className={classes.content}>
-          <h2 className={classes.title}>75% of our clients hire the first match we make!</h2>
-          <p className={classes.paragraph}>
-            And this is possible only because we love what we do and strive for excellence in everything we do.
-          </p>
-          <p className={classes.paragraph}>
-            Our staff works as a dream team where everyone is crucial player in the hiring journey.
-          </p>
-        </div>
+        <TabContainer
+          tabListClassName={classes.tabsList}
+          tabContainerClassName={classes.tabsContainer}
+          tabPanelClassName={classes.tabContent}
+          tabButtonClassName={classes.tabButton}
+          tabHeaderClassName={classes.tabHeader}
+          selectedTabClassName={classes.selectedTab}
+          tabs={tabs}
+        />
       </section>
       <BackgroundContainer backgroundURL={NumbersSpeakBackground} />
     </>
