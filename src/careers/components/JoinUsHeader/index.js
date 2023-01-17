dc2ae29a4dc07b6807b9407e82ed734c39abf9bd
adrian <adrian.scicchitano@wedevelop.me>
2023-01-17 09:13:34 -0300

    change job openings wording

--- src/careers/components/JoinUsHeader/index.js
+++ src/careers/components/JoinUsHeader/index.js
@@ -5,7 +5,11 @@ export default function JoinUsHeader () {
     <section>
       <p className={classes.subheadingText}>Join Us</p>
       <h2 className={classes.titleText}>Job Openings</h2>
-      <p className={classes.description}>Didn’t find a job for your profile? Leave your information and join our talent pool.</p>
+      <p className={classes.description}>
+        Unlock global opportunities and take your IT career to the next level by
+        joining our remote team today! Collaborate with experts from Latin America
+        and work on cutting-edge projects based in the United States
+      </p>
       <hr className={classes.horizontalBar} />
     </section>
   )
